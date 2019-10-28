d03ed3f2de07aa17c86ae8c0014cdf36d023b5ce
AlexisRalli <rallilex@hotmail.co.uk>
2019-10-28 11:51:03 +0000

    graph class builds both indexed and string graph... need to clean up

--- Graph.py
+++ Graph.py
@@ -14,10 +14,13 @@ class BuildGraph():
         self.G_string = nx.Graph() # undirected graph
         self.node_string_set = None
         self.G_string_comp = None
+        self.greedy_string = None
+        self.colour_key_for_nodes_string = None
 
         self.G_index = nx.Graph()
         self.node_index_set = None
         self.G_index_comp = None
+        self.greedy_index = None
 
 
     def Get_node_terms_as_strings(self):
@@ -39,50 +42,175 @@ class BuildGraph():
 
 
 
-    def Build_nodes(self, string_nodes = True, index_nodes = False, plot_graph = False):
+    def Build_string_nodes(self, plot_graph = False):
 
-        if string_nodes == True:
-            for string_node in self.node_string_set:
-                self.G.add_node(string_node)
+        if self.node_string_set == None:
+            self.Get_node_terms_as_strings()
 
-        elif index_nodes == True:
-            for numerical_node in self.node_index_set:
-                self.G.add_node(numerical_node)
-
-
+        for string_node in self.node_string_set:
+            self.G_string.add_node(string_node)
 
 
         if plot_graph == True:
             plt.figure()
-            nx.draw(self.G, with_labels=1)
+            nx.draw(self.G_string, with_labels=1)
+            plt.show()
+
+    def Build_index_nodes(self, plot_graph = False):
+
+        if self.node_index_set == None:
+            self.Get_nodes_terms_as_indices()
+
+        for numerical_node in self.node_index_set:
+            self.G_index.add_node(numerical_node)
+
+
+        if plot_graph == True:
+            plt.figure()
+            nx.draw(self.G_index, with_labels=1)
             plt.show()
 
 
 
-    def Build_edges(self, plot_graph = False):
+    def Build_string_edges(self, plot_graph = False):
 
-        nodes_list = list(self.G.nodes())
+        if len(self.G_string.nodes()) == 0:
+            self.Build_string_nodes()
+
+        nodes_list = list(self.G_string.nodes())
         for index, commuting_indices in self.indices:
             for commuting_index in commuting_indices:
                 if commuting_index != []:
-                    self.G.add_edge(nodes_list[index], nodes_list[commuting_index])
+                    self.G_string.add_edge(nodes_list[index], nodes_list[commuting_index])
 
         if plot_graph == True:
             plt.figure()
-            pos = nx.circular_layout(self.G)
-            nx.draw(self.G, pos, with_labels=1)
+            pos = nx.circular_layout(self.G_string)
+            nx.draw(self.G_string, pos, with_labels=1)
             plt.show()
 
 
-    def Get_complementary_graph(self, plot_graph = False):
+    def Build_index_edges(self, plot_graph=False):
 
-        self.G_comp = nx.complement(self.G)
+        if len(self.G_index.nodes()) == 0:
+            self.Build_index_nodes()
+
+        for index, commuting_indices in self.indices:
+            for i in range(len(commuting_indices)):
+
+                if commuting_indices[i] != []:
+                    self.G_index.add_edge(index, commuting_indices[i])
 
         if plot_graph == True:
             plt.figure()
-            pos = nx.circular_layout(self.G_comp)
-            nx.draw(self.G_comp, pos, with_labels=1)
+            pos = nx.circular_layout(self.G_index)
+            nx.draw(self.G_index, pos, with_labels=1)
             plt.show()
+
+
+
+    def Get_complementary_graph_string(self, plot_graph = False):
+
+        if len(list(self.G_string.edges())) == 0:
+            self.Build_string_edges()
+
+        self.G_string_comp = nx.complement(self.G_string)
+
+        if plot_graph == True:
+            plt.figure()
+            pos = nx.circular_layout(self.G_string_comp)
+            nx.draw(self.G_string_comp, pos, with_labels=1)
+            plt.show()
+
+    def Get_complementary_graph_index(self, plot_graph = False):
+
+        if len(list(self.G_index.edges())) == 0:
+            self.Build_index_edges()
+
+        self.G_index_comp = nx.complement(self.G_index)
+
+        if plot_graph == True:
+            plt.figure()
+            pos = nx.circular_layout(self.G_index_comp)
+            nx.draw(self.G_index_comp, pos, with_labels=1)
+            plt.show()
+
+
+    def colour_string_graph(self, strategy = 'largest_first'):
+
+        if self.G_string_comp == None:
+            self.Get_complementary_graph_string()
+
+        self.greedy_string = nx.greedy_color(self.G_string_comp, strategy=strategy, interchange=False)
+
+    def colour_index_graph(self, strategy='largest_first'):
+
+        if self.G_index_comp == None:
+            self.Get_complementary_graph_index()
+
+        self.greedy_index = nx.greedy_color(self.G_index_comp, strategy=strategy, interchange=False)
+
+
+    def Get_coloured_keys_string(self, plot_graph = False):
+
+
+        # store the names (the keys of the new dict) as a set (keeps elements unique)
+        unique_colours = set(self.greedy_string.values())
+
+        colour_key_for_nodes_string = {}
+        for colour in unique_colours:
+            colour_key_for_nodes_string[colour] = [k for k in self.greedy_string.keys()
+                                                   if self.greedy_string[k] == colour]
+        self.colour_key_for_nodes_string = colour_key_for_nodes_string
+
+        if plot_graph == True:
+            import matplotlib.cm as cm
+            plt.figure()
+            colour_list = cm.rainbow(np.linspace(0, 1, len(self.colour_key_for_nodes_string)))
+            pos = nx.circular_layout(self.G_string_comp)
+
+            for colour in self.colour_key_for_nodes_string:
+                nx.draw_networkx_nodes(self.G_string_comp, pos,
+                                       nodelist=self.colour_key_for_nodes_string[colour],
+                                       node_color=colour_list[colour],
+                                       node_size=500,
+                                       alpha=0.8)
+
+            nx.draw_networkx_edges(self.G_string_comp, pos, width=1.0, alpha=0.5)
+
+            # need to get FONT to change! TODO
+            nx.draw_networkx_labels(self.G_string_comp, pos, font_family='Times-New-Roman', font_size=12)
+            plt.plot()
+
+    def Get_coloured_keys_index(self, plot_graph=False):
+        # store the names (the keys of the new dict) as a set (keeps elements unique)
+        unique_colours = set(self.greedy_index.values())
+
+        # use a list comprehension, iterating through keys and checking the values match each k
+        colour_key_for_nodes_index = {}
+        for colour in unique_colours:
+            colour_key_for_nodes_index[colour] = [k for k in self.greedy_index.keys()
+                                                  if self.greedy_index[k] == colour]
+        self.colour_key_for_nodes_index = colour_key_for_nodes_index
+
+        if plot_graph == True:
+            import matplotlib.cm as cm
+
+            plt.figure()
+            colour_list = cm.rainbow(np.linspace(0, 1, len(self.colour_key_for_nodes_index)))
+            pos = nx.circular_layout(self.G_index_comp)
+
+            for colour in self.colour_key_for_nodes_index:
+                nx.draw_networkx_nodes(self.G_index_comp, pos,
+                                       nodelist=self.colour_key_for_nodes_index[colour],
+                                       node_color=colour_list[colour],
+                                       node_size=500,
+                                       alpha=0.8)
+
+            nx.draw_networkx_edges(self.G_index_comp, pos, width=1.0, alpha=0.5)
+
+            nx.draw_networkx_labels(self.G_index_comp, pos)
+            plt.plot()
 
 
 if __name__ == '__main__':
@@ -116,10 +244,17 @@ if __name__ == '__main__':
      (12, [0, 1, 2, 3, 4, 5, [], [], [], [], 10, 11, 13, 14]),
      (13, [0, 1, 2, 3, 4, 5, [], [], [], [], 10, 11, 12, 14]),
      (14, [0, 1, 2, 3, 4, 5, [], [], [], [], 10, 11, 12, 13])]
+
     Y = BuildGraph(PauliWords, indices)
-    Y.Get_node_terms_as_strings()
-    print(Y.node_string_set)
-    Y.Build_nodes(plot_graph=True)
-    Y.Build_edges(plot_graph=True)
-    Y.Get_complementary_graph(plot_graph=True)
+    Y.Build_index_nodes()# plot_graph=True)
+    Y.Build_index_edges()# plot_graph=True)
+    Y.Get_complementary_graph_index() #plot_graph=True)
+    Y.colour_index_graph()
+    Y.Get_coloured_keys_index(plot_graph=True)
+
+    Y.Build_string_nodes()# plot_graph=True)
+    Y.Build_string_edges()# plot_graph=True)
+    Y.Get_complementary_graph_string()# plot_graph=True)
+    Y.colour_string_graph()
+    Y.Get_coloured_keys_string(plot_graph=True)
 
