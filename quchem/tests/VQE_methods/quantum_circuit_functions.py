1ac01daa347caf078a80c5fba355c8ce35a66fd6
AlexisRalli <rallilex@hotmail.co.uk>
2019-11-13 17:49:14 +0000

    have addded new quantum circuit functions... this builds an exponentiated operator of anything of the form: (PauliWord, constant) and angle to rotate

---
+++ quchem/tests/VQE_methods/quantum_circuit_functions.py
@@ -0,0 +1,363 @@
+import cirq
+import numpy as np
+
+class State_Prep(cirq.Gate):
+
+    def __init__(self, initial_state):
+        """""
+    Circuit to obtain given initial state
+    e.g. |0011>  =  [0,0,1,1]
+
+    :param initial_state: A list description of HF state... note that indexing from far right to left.
+    :type initial_state: list, (numpy.array, tuple)
+
+    ...
+    :raises [ErrorType]: [ErrorDescription]
+    ...
+    :return: A circuit object to be used by cirq.Circuit.from_ops
+    :rtype: class
+
+        """
+        self.state = initial_state
+
+
+    def _decompose_(self, qubits):
+
+        for i in range(len(self.state)):
+            state = self.state[i]
+            qubitNo = i
+
+            if state == 1:
+                yield cirq.X(qubits[qubitNo])
+
+            if state != 0 and state !=1:
+                raise ValueError('initial state not in correct format... qubit {} has value {} ' \
+                                 '[instead of 0 or 1]'.format(i, state))
+
+    def num_qubits(self):
+        return len(self.state)
+
+    def _circuit_diagram_info_(self, args):
+        state_prep_list = []
+        for i in range(len(self.state)):
+            state = self.state[i]
+            if state == 1:
+                state_prep_list.append('state_prep: |1> ')
+            elif state == 0:
+                state_prep_list.append('state_prep: |0> ')
+            else:
+                raise ValueError('state needs to be list of 0 or 1 s ' \
+                                 'qubit {} has value {}'.format(i, state))
+        return state_prep_list
+
+
+    def Return_circuit_as_list(self):
+        circuit_list=[]
+        for i in range(len(self.state)):
+            state = self.state[i]
+            qubitNo = i
+            if state == 1:
+                circuit_list.append(cirq.X(qubitNo))
+        return circuit_list
+
+# Test
+if __name__ == '__main__':
+    initial_state = State_Prep([0,0,1,1])
+    print(cirq.Circuit.from_ops((initial_state(*cirq.LineQubit.range(initial_state.num_qubits())))))
+    print(
+        cirq.Circuit.from_ops(cirq.decompose_once((initial_state(*cirq.LineQubit.range(initial_state.num_qubits()))))))
+
+##
+class Change_of_Basis_initial(cirq.Gate):
+    def __init__(self, PauliWord_and_cofactor):
+        """
+         Circuit to perform change of basis in order to perform: e^(-i theta_sk/2 X_sk)
+         This cirq gate changes to correct basis.
+
+        :param PauliWord_and_cofactor: (PauliWord, constant)
+        :type PauliWord_and_cofactor: tuple
+
+        e.g.: ('X0 X1 X2 Y3', -0.28527408634774526j)
+
+        ...
+        :raises [ErrorType]: [ErrorDescription]
+        ...
+        :return: A circuit object to be used by cirq.Circuit.from_ops
+        :rtype: class
+       """
+
+        self.PauliWord_and_cofactor = PauliWord_and_cofactor
+
+    def _decompose_(self, qubits):
+
+        PauliWord = self.PauliWord_and_cofactor[0].split(' ')
+
+        for PauliString in PauliWord:
+            qubitOp = PauliString[0]
+            qubitNo = int(PauliString[1::])
+
+            if qubitOp == 'X':
+                yield cirq.H(qubits[qubitNo])
+            elif qubitOp == 'Y':
+                 yield cirq.Rx(np.pi / 2)(qubits[qubitNo])
+            elif qubitOp == 'Z' or 'I':
+                continue
+            else:
+                raise ValueError("Qubit Operation: {} is NOT a Pauli operation".format(qubitOp))
+
+    def _circuit_diagram_info_(self, args):
+        Ansatz_basis_change_list = []
+        PauliWord = self.PauliWord_and_cofactor[0].split(' ')
+        for i in range(len(PauliWord)):
+                Ansatz_basis_change_list.append('Basis_change')
+        return Ansatz_basis_change_list
+
+    def num_qubits(self):
+        PauliWord = self.PauliWord_and_cofactor[0].split(' ')
+        return len(PauliWord)
+
+if __name__ == '__main__':
+    PauliWord_test = ('Z0 X1 Y2 Z3 I4 I5 I6 I7 I8 Y9 X10', (0.8918294488900189+0j))
+
+    Basis_change_circuit = Change_of_Basis_initial(PauliWord_test)
+
+    print(cirq.Circuit.from_ops((Basis_change_circuit(*cirq.LineQubit.range(Basis_change_circuit.num_qubits())))))
+    print(
+        cirq.Circuit.from_ops(cirq.decompose_once((Basis_change_circuit(*cirq.LineQubit.range(Basis_change_circuit.num_qubits()))))))
+
+class Engtangle_initial(cirq.Gate):
+    def __init__(self, PauliWord_and_cofactor):
+        """
+         Circuit to perform change of basis in order to perform: e^(-i theta_sk/2 X_sk)
+         This cirq gate entangles qubits prior to rotation
+
+        :param PauliWord_and_cofactor: (PauliWord, constant)
+        :type PauliWord_and_cofactor: tuple
+
+        e.g.: ('X0 X1 X2 Y3', -0.28527408634774526j)
+
+        ...
+        :raises [ErrorType]: [ErrorDescription]
+        ...
+        :return: A circuit object to be used by cirq.Circuit.from_ops
+        :rtype: class
+       """
+        self.PauliWord_and_cofactor = PauliWord_and_cofactor
+
+    def _decompose_(self, qubits):
+
+        PauliWord = self.PauliWord_and_cofactor[0].split(' ')
+
+        # note identity terms removed here
+        qubitNo_qubitOp_list = [(int(PauliString[1::]), PauliString[0]) for PauliString in PauliWord if PauliString[0] != 'I']
+
+        control_qubit = max([qubitNo for qubitNo, qubitOp in qubitNo_qubitOp_list])
+
+        for j in range(len(qubitNo_qubitOp_list)):
+            qubitNo = qubitNo_qubitOp_list[j][0]
+            #qubitOp = qubitNo_qubitOp_list[j][1]
+
+            if qubitNo < control_qubit:
+                qubitNo_NEXT = qubitNo_qubitOp_list[j + 1][0]
+                yield cirq.CNOT(qubits[qubitNo], qubits[qubitNo_NEXT])
+
+
+    def _circuit_diagram_info_(self, args):
+        string_list = []
+        PauliWord = self.PauliWord_and_cofactor[0].split(' ')
+        for i in range(len(PauliWord)):
+                string_list.append('Entangling circuit')
+        return string_list
+
+
+    def num_qubits(self):
+        PauliWord = self.PauliWord_and_cofactor[0].split(' ')
+        return len(PauliWord)
+
+if __name__ == '__main__':
+    PauliWord_test = ('Z0 X1 Y2 Z3 I4 I5 I6 I7 I8 Y9 X10', (0.8918294488900189+0j))
+    Ent_initial = Engtangle_initial(PauliWord_test)
+
+    print(cirq.Circuit.from_ops((Ent_initial(*cirq.LineQubit.range(Ent_initial.num_qubits())))))
+    print(
+        cirq.Circuit.from_ops(cirq.decompose_once((Ent_initial(*cirq.LineQubit.range(Ent_initial.num_qubits()))))))
+
+class PauliWord_exponential_rotation(cirq.Gate):
+
+    def __init__(self, PauliWord_and_cofactor, theta):
+        """
+         Circuit to perform change of basis in order to perform: e^(-i theta_sk/2 X_sk)
+         This cirq gate entangles qubits prior to rotation
+
+        :param PauliWord_and_cofactor: (PauliWord, constant)
+        :type PauliWord_and_cofactor: tuple
+
+         e.g.: ('X0 X1 X2 Y3', -0.28527408634774526j)
+
+        :param theta: angle to rotate by
+        :type theta: float
+
+        ...
+        :raises [ErrorType]: [ErrorDescription]
+        ...
+        :return: A circuit object to be used by cirq.Circuit.from_ops
+        :rtype: class
+       """
+        self.PauliWord_and_cofactor = PauliWord_and_cofactor
+        self.theta = theta
+
+
+    def _decompose_(self, qubits):
+
+        PauliWord = self.PauliWord_and_cofactor[0].split(' ')
+
+        # note identity terms removed here
+        qubitNo_qubitOp_list = [(int(PauliString[1::]), PauliString[0]) for PauliString in PauliWord if PauliString[0] != 'I']
+
+        control_qubit = max([qubitNo for qubitNo, qubitOp in qubitNo_qubitOp_list])
+
+        yield cirq.Rz(self.theta).on(qubits[control_qubit])
+
+    def num_qubits(self):
+        PauliWord = self.PauliWord_and_cofactor[0].split(' ')
+        return len(PauliWord)
+
+    def _circuit_diagram_info_(self, args):
+        string_list = []
+        PauliWord = self.PauliWord_and_cofactor[0].split(' ')
+        for i in range(len(PauliWord)):
+                string_list.append('R_sk_rotation circuit')
+        return string_list
+
+if __name__ == '__main__':
+    PauliWord_test = ('Z0 X1 Y2 Z3 I4 I5 I6 I7 I8 Y9 X10', (0.8918294488900189+0j))
+    theta = np.pi
+
+    Rzz = PauliWord_exponential_rotation(PauliWord_test, theta)
+
+    print(cirq.Circuit.from_ops((Rzz(*cirq.LineQubit.range(Rzz.num_qubits())))))
+    print(
+        cirq.Circuit.from_ops(cirq.decompose_once((Rzz(*cirq.LineQubit.range(Rzz.num_qubits()))))))
+
+class Engtangle_final(cirq.Gate):
+    def __init__(self, PauliWord_and_cofactor):
+        """
+         Circuit to perform change of basis in order to perform: e^(-i theta_sk/2 X_sk)
+         This cirq gate entangles qubits post rotation
+
+        :param PauliWord_and_cofactor: (PauliWord, constant)
+        :type PauliWord_and_cofactor: tuple
+
+        e.g.: ('X0 X1 X2 Y3', -0.28527408634774526j)
+
+        ...
+        :raises [ErrorType]: [ErrorDescription]
+        ...
+        :return: A circuit object to be used by cirq.Circuit.from_ops
+        :rtype: class
+       """
+        self.PauliWord_and_cofactor = PauliWord_and_cofactor
+
+    def _decompose_(self, qubits):
+
+        PauliWord = self.PauliWord_and_cofactor[0].split(' ')
+
+        # note identity terms removed here
+        qubitNo_qubitOp_list_REVERSE = [(int(PauliString[1::]), PauliString[0]) for PauliString in PauliWord if PauliString[0] != 'I'][::-1]
+
+        control_qubit = max([qubitNo for qubitNo, qubitOp in qubitNo_qubitOp_list_REVERSE])
+
+        for i in range(len(qubitNo_qubitOp_list_REVERSE)):
+            qubitNo, qubitOp = qubitNo_qubitOp_list_REVERSE[i]
+
+            if qubitNo < control_qubit and qubitNo >= 0:
+                qubitNo_NEXT = qubitNo_qubitOp_list_REVERSE[i - 1][0]   # note negative here
+                yield cirq.CNOT(qubits[qubitNo], qubits[qubitNo_NEXT])
+
+
+    def _circuit_diagram_info_(self, args):
+        string_list = []
+        PauliWord = self.PauliWord_and_cofactor[0].split(' ')
+        for i in range(len(PauliWord)):
+                string_list.append('Entangling circuit')
+        return string_list
+
+
+    def num_qubits(self):
+        PauliWord = self.PauliWord_and_cofactor[0].split(' ')
+        return len(PauliWord)
+
+
+if __name__ == '__main__':
+    PauliWord_test = ('Z0 X1 Y2 Z3 I4 I5 I6 I7 I8 Y9 X10', (0.8918294488900189+0j))
+    Ent_final = Engtangle_final(PauliWord_test)
+
+    print(cirq.Circuit.from_ops((Ent_final(*cirq.LineQubit.range(Ent_final.num_qubits())))))
+    print(
+        cirq.Circuit.from_ops(cirq.decompose_once((Ent_final(*cirq.LineQubit.range(Ent_final.num_qubits()))))))
+
+
+class full_exponentiated_PauliWord_circuit(cirq.Gate):
+    def __init__(self, PauliWord_and_cofactor, theta):
+        """
+        This circuit takes in PauliWord and angle and performs: e^(-i theta_sk/2 X_sk)
+
+        :param PauliWord_and_cofactor: (PauliWord, constant)
+        :type PauliWord_and_cofactor: tuple
+
+        :param theta: angle to rotate by
+        :type theta: float
+
+        e.g.: ('X0 X1 X2 Y3', -0.28527408634774526j)
+
+        ...
+        :raises [ErrorType]: [ErrorDescription]
+        ...
+        :return: A circuit object to be used by cirq.Circuit.from_ops
+        :rtype: class
+       """
+        self.PauliWord_and_cofactor = PauliWord_and_cofactor
+        self.theta = theta
+
+
+    def _decompose_(self, qubits):
+
+
+        Basis_change_circuit = Change_of_Basis_initial(self.PauliWord_and_cofactor)
+        Ent_initial = Engtangle_initial(self.PauliWord_and_cofactor)
+        PauliWord_exponential_rotation_circuit = PauliWord_exponential_rotation(self.PauliWord_and_cofactor, self.theta)
+        Ent_final = Engtangle_final(self.PauliWord_and_cofactor)
+
+        basis_change_initial_gen = Basis_change_circuit._decompose_(qubits)
+        Ent_initial_gen = Ent_initial._decompose_(qubits)
+        PauliWord_exponential_rotation_circuit_gen = PauliWord_exponential_rotation_circuit._decompose_(qubits)
+        Ent_final_gen = Ent_final._decompose_(qubits)
+        basis_change_final_gen = Basis_change_circuit._decompose_(qubits)
+
+        list_generators = [basis_change_initial_gen, Ent_initial_gen, PauliWord_exponential_rotation_circuit_gen, Ent_final_gen,
+                           basis_change_final_gen]
+        yield list_generators
+
+
+
+    def _circuit_diagram_info_(self, args):
+        string_list = []
+        PauliWord = self.PauliWord_and_cofactor[0].split(' ')
+        for i in range(len(PauliWord)):
+                string_list.append('full_exp_PauliWord_circuit')
+        return string_list
+
+    def num_qubits(self):
+        PauliWord = self.PauliWord_and_cofactor[0].split(' ')
+        return len(PauliWord)
+
+if __name__ == '__main__':
+    PauliWord_test = ('Z0 X1 Y2 Z3 I4 I5 I6 I7 I8 Y9 X10', (0.8918294488900189+0j))
+    theta = np.pi
+
+    full_Oper = full_exponentiated_PauliWord_circuit(PauliWord_test, theta)
+
+    print(cirq.Circuit.from_ops((full_Oper(*cirq.LineQubit.range(full_Oper.num_qubits())))))
+    print(
+        cirq.Circuit.from_ops(
+            cirq.decompose_once((full_Oper(*cirq.LineQubit.range(full_Oper.num_qubits()))))))
