01cae3d84dc5af2ce5575d10f29d45d3785c9f2e
AlexisRalli <rallilex@hotmail.co.uk>
2019-11-19 14:59:56 +0000

    Written all circuit simulation stuff. NEED to now look at optimizers!
    ALSO still need to look at making code that gives circuits_and_constants into a FUNCTION or CLASS. Have written code for this in Unitary_partitioning file BUT this breaks the projet_measurment_reduction file. NEED TO LOOK AT THIS!

--- quchem/tests/VQE_methods/Simulating_Quantum_Circuit.py
+++ quchem/tests/VQE_methods/Simulating_Quantum_Circuit.py
@@ -187,12 +187,103 @@ def Simulate_Quantum_Circuit(quantum_circuit_dict, num_shots):
     results={}
     for key in quantum_circuit_dict:
         if quantum_circuit_dict[key]['circuit'] == None:
-            continue
+            identity_circuit = (quantum_circuit_dict[key]['PauliWord'], quantum_circuit_dict[key]['factor'])
         else:
             circuit = quantum_circuit_dict[key]['circuit']
-            raw_result =  simulator.run(circuit, repetitions=num_shots)
-
+            raw_result = simulator.run(circuit, repetitions=num_shots)
             results[key] =raw_result.histogram(key=histogram_strings[key])
-    return results
 
 
+    return results,identity_circuit
+
+
+def Return_as_binary(histogram_results, quantum_circuit_dict):
+    """
+
+    e.g.
+    {1: Counter({3: 798, 0: 202}),
+     2: Counter({2: 806, 1: 194}),
+     3: Counter({1: 219, 2: 781}),
+     4: Counter({2: 773, 1: 227}),
+     5: Counter({1: 198, 2: 802}),
+     6: Counter({3: 199, 0: 801}),
+     7: Counter({1: 907, 0: 93}),
+     8: Counter({1: 911, 0: 89}),
+     9: Counter({0: 929, 1: 71}),
+     10: Counter({0: 361, 1: 639})}
+
+    :return:
+    e.g.
+    {1: {'11': 798, '00': 202},
+     2: {'10': 806, '01': 194},
+     3: {'01': 219, '10': 781},
+     4: {'10': 773, '01': 227},
+     5: {'01': 198, '10': 802},
+     6: {'11': 199, '00': 801},
+     7: {'1': 907, '0': 93},
+     8: {'1': 911, '0': 89},
+     9: {'0': 929, '1': 71},
+     10: {'0': 361, '1': 639}}
+    """
+
+    results_binary={}
+    for KEY in histogram_results:
+        result_instance = histogram_results[KEY]
+
+        PauliWord = quantum_circuit_dict[KEY]['PauliWord']
+        PauliWord = PauliWord.split(' ')
+        num_terms_to_measure = len([i for i in PauliWord if i[0] != 'I'])
+
+        temp_result = {}
+        for output_state in result_instance:
+            binary_length = '{' +'0:0{}b'.format(num_terms_to_measure) + '}'
+
+            binary_key = binary_length.format(output_state)
+
+            temp_result[binary_key] = result_instance[output_state]
+
+        results_binary[KEY] = {'Counter': temp_result, 'factor': quantum_circuit_dict[KEY]['factor']}
+    return results_binary
+
+
+def expectation_value(results_binary):
+    """
+
+    < Z >  = (num_0 - num_1) / total_num_measurements = (num_0 - num_1) / (num_0 + num_1)
+
+    :return:
+    """
+    expectation_value_results ={}
+    for key in results_binary:
+        num_ones = 0
+        num_zeros = 0
+        for state in results_binary[key]['Counter']:
+            individual_outcomes = [int(bit) for bit in state]
+            print(individual_outcomes)
+            for bit in individual_outcomes:
+                if bit == 1:
+                    num_ones += results_binary[key]['Counter'][state]
+                elif bit == 0:
+                    num_zeros += results_binary[key]['Counter'][state]
+                else:
+                    raise ValueError('incorrect outcome {}'.format(individual_outcomes))
+        print('number of 1 s =', num_ones, 'number of 0 s =', num_zeros)
+        expectation_value_results[key] = (num_zeros - num_ones) / (num_zeros + num_ones) * results_binary[key]['factor']
+
+    return expectation_value_results
+
+
+
+def Calc_Energy(quantum_circuit_dict, num_shots):
+    histogram_results, identity_result = Simulate_Quantum_Circuit(quantum_circuit_dict, num_shots)
+    results_binary = Return_as_binary(histogram_results, quantum_circuit_dict)
+    exp_per_circuit = expectation_value(results_binary)
+
+    I_result = identity_result[1]
+
+    E= I_result
+    for circuit in exp_per_circuit:
+        E += exp_per_circuit[circuit]
+    return E
+
+Calc_Energy(circuits_and_constants, 1000)
