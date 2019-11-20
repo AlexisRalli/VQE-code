66391764f2c8d3b602ba2eec27ded7c3dbd5c60b
AlexisRalli <rallilex@hotmail.co.uk>
2019-11-20 18:47:38 +0000

    look at Unitary_partitioning script lines 887 and 889... for the iX_sk constant I am taking the prodct of each constant defined by eq (11) in arxiv paper. NOT sure if this is correct! Need to look closely at this!

--- quchem/tests/VQE_methods/quantum_circuit_functions.py
+++ quchem/tests/VQE_methods/quantum_circuit_functions.py
@@ -287,7 +287,6 @@ class Engtangle_final(cirq.Gate):
         PauliWord = self.PauliWord_and_cofactor[0].split(' ')
         return len(PauliWord)
 
-
 if __name__ == '__main__':
     PauliWord_test = ('Z0 X1 Y2 Z3 I4 I5 I6 I7 I8 Y9 X10', (0.8918294488900189+0j))
     Ent_final = Engtangle_final(PauliWord_test)
@@ -296,6 +295,62 @@ if __name__ == '__main__':
     print(
         cirq.Circuit.from_ops(cirq.decompose_once((Ent_final(*cirq.LineQubit.range(Ent_final.num_qubits()))))))
 
+class Change_of_Basis_final(cirq.Gate):
+    def __init__(self, PauliWord_and_cofactor):
+        """
+         Circuit to perform change of basis in order to perform: e^(-i theta_sk/2 X_sk)
+         This cirq gate changes to correct basis.
+
+        :param PauliWord_and_cofactor: (PauliWord, constant)
+        :type PauliWord_and_cofactor: tuple
+
+        e.g.: ('X0 X1 X2 Y3', -0.28527408634774526j)
+
+        ...
+        :raises [ErrorType]: [ErrorDescription]
+        ...
+        :return: A circuit object to be used by cirq.Circuit.from_ops
+        :rtype: class
+       """
+
+        self.PauliWord_and_cofactor = PauliWord_and_cofactor
+
+    def _decompose_(self, qubits):
+
+        PauliWord = self.PauliWord_and_cofactor[0].split(' ')
+
+        for PauliString in PauliWord:
+            qubitOp = PauliString[0]
+            qubitNo = int(PauliString[1::])
+
+            if qubitOp == 'X':
+                yield cirq.H(qubits[qubitNo])
+            elif qubitOp == 'Y':
+                 yield cirq.Rx(-np.pi / 2)(qubits[qubitNo])
+            elif qubitOp == 'Z' or 'I':
+                continue
+            else:
+                raise ValueError("Qubit Operation: {} is NOT a Pauli operation".format(qubitOp))
+
+    def _circuit_diagram_info_(self, args):
+        Ansatz_basis_change_list = []
+        PauliWord = self.PauliWord_and_cofactor[0].split(' ')
+        for i in range(len(PauliWord)):
+                Ansatz_basis_change_list.append('Basis_change')
+        return Ansatz_basis_change_list
+
+    def num_qubits(self):
+        PauliWord = self.PauliWord_and_cofactor[0].split(' ')
+        return len(PauliWord)
+
+if __name__ == '__main__':
+    PauliWord_test = ('Z0 X1 Y2 Z3 I4 I5 I6 I7 I8 Y9 X10', (0.8918294488900189+0j))
+
+    Basis_change_circuit = Change_of_Basis_final(PauliWord_test)
+
+    print(cirq.Circuit.from_ops((Basis_change_circuit(*cirq.LineQubit.range(Basis_change_circuit.num_qubits())))))
+    print(
+        cirq.Circuit.from_ops(cirq.decompose_once((Basis_change_circuit(*cirq.LineQubit.range(Basis_change_circuit.num_qubits()))))))
 
 class full_exponentiated_PauliWord_circuit(cirq.Gate):
     def __init__(self, PauliWord_and_cofactor, theta):
@@ -323,16 +378,17 @@ class full_exponentiated_PauliWord_circuit(cirq.Gate):
     def _decompose_(self, qubits):
 
 
-        Basis_change_circuit = Change_of_Basis_initial(self.PauliWord_and_cofactor)
+        Basis_change_initial_circuit = Change_of_Basis_initial(self.PauliWord_and_cofactor)
         Ent_initial = Engtangle_initial(self.PauliWord_and_cofactor)
         PauliWord_exponential_rotation_circuit = PauliWord_exponential_rotation(self.PauliWord_and_cofactor, self.theta)
         Ent_final = Engtangle_final(self.PauliWord_and_cofactor)
+        Basis_change_final_circuit = Change_of_Basis_final(self.PauliWord_and_cofactor)
 
-        basis_change_initial_gen = Basis_change_circuit._decompose_(qubits)
+        basis_change_initial_gen = Basis_change_initial_circuit._decompose_(qubits)
         Ent_initial_gen = Ent_initial._decompose_(qubits)
         PauliWord_exponential_rotation_circuit_gen = PauliWord_exponential_rotation_circuit._decompose_(qubits)
         Ent_final_gen = Ent_final._decompose_(qubits)
-        basis_change_final_gen = Basis_change_circuit._decompose_(qubits)
+        basis_change_final_gen = Basis_change_final_circuit._decompose_(qubits)
 
         list_generators = [basis_change_initial_gen, Ent_initial_gen, PauliWord_exponential_rotation_circuit_gen, Ent_final_gen,
                            basis_change_final_gen]
