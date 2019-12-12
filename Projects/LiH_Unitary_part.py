397ffbaa43d4e1313f3e1ab89d88342a28cd2571
AlexisRalli <rallilex@hotmail.co.uk>
2019-12-12 17:30:56 +0000

    started looking at graph coloring problem... and optimum P_s selection

---
+++ Projects/LiH_Unitary_part.py
@@ -0,0 +1,36 @@
+from quchem.Hamiltonian_Generator_Functions import Hamiltonian
+from quchem.Graph import BuildGraph_string
+from quchem.Unitary_partitioning import *
+
+
+### Variable Parameters
+Molecule = 'H2'#'LiH'
+geometry = None
+#n_electrons = 2
+num_shots = 10000
+####
+
+### Get Hamiltonian
+Hamilt = Hamiltonian(Molecule,
+                     run_scf = 1, run_mp2 = 1, run_cisd = 0, run_ccsd = 0, run_fci = 1,
+                 basis = 'sto-3g',
+                 multiplicity = 1,
+                 geometry = geometry) # normally None!
+
+Hamilt.Get_all_info(get_FCI_energy=False)
+
+
+
+Commuting_indices = Hamilt.Commuting_indices
+PauliWords = Hamilt.QubitHamiltonianCompleteTerms
+constants = Hamilt.HamiltonainCofactors
+
+
+### Get anti-commutativity graph
+HamiltGraph = BuildGraph_string(PauliWords, Commuting_indices, constants)
+HamiltGraph.Build_string_nodes()  # plot_graph=True)
+HamiltGraph.Build_string_edges()  # plot_graph=True)
+HamiltGraph.Get_complementary_graph_string()  # plot_graph=True)
+HamiltGraph.colouring(plot_graph=False)
+
+anti_commuting_sets = HamiltGraph.anticommuting_sets
