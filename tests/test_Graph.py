cd97030dee95ee7510c1f3ec32e14b0586f96943
AlexisRalli <44055008+AlexisRalli@users.noreply.github.com>
2019-12-19 16:50:41 +0000

    a few graph tests added

--- tests/test_Graph.py
+++ tests/test_Graph.py
@@ -1,5 +1,7 @@
-from quchem.Graph import BuildGraph_string
+from quchem.Graph import *
 import pytest
+
+# py.test test_Graph.py -vv
 
 PauliWords = [[(0, 'I'), (1, 'I'), (2, 'I'), (3, 'I')],
               [(0, 'Z'), (1, 'I'), (2, 'I'), (3, 'I')],
@@ -32,16 +34,46 @@ indices = [(0, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]),
            (13, [0, 1, 2, 3, 4, 5, [], [], [], [], 10, 11, 12, 14]),
            (14, [0, 1, 2, 3, 4, 5, [], [], [], [], 10, 11, 12, 13])]
 
+def test_Get_PauliWords_as_nodes_NORMAL():
+    # normal use case
+    List_PauliWords = [
+        [(0, 'I'), (1, 'I'), (2, 'I'), (3, 'I')],
+        [(0, 'Z'), (1, 'I'), (2, 'I'), (3, 'I')],
+        [(0, 'I'), (1, 'Z'), (2, 'I'), (3, 'I')],
+        [(0, 'I'), (1, 'I'), (2, 'I'), (3, 'Z')],
+        [(0, 'I'), (1, 'I'), (2, 'Z'), (3, 'Z')]
+    ]
+    expected_output = ['I0 I1 I2 I3', 'Z0 I1 I2 I3', 'I0 Z1 I2 I3', 'I0 I1 I2 Z3', 'I0 I1 Z2 Z3']
 
-def test_Get_nodes_terms_as_indices():
-    '''
+    output = Get_PauliWords_as_nodes(List_PauliWords)
+    assert(output == expected_output)
 
-    :return:
-    '''
+def test_Get_list_of_nodes_and_attributes_NORMAL():
 
-    graph = BuildGraph_string(PauliWords, indices)
-    graph.Build_index_nodes()
+    List_of_nodes =  [
+                        'I0 I1 I2 I3',
+                        'Z0 I1 I2 I3',
+                        'I0 Z1 I2 I3',
+                        'I0 I1 I2 Z3',
+                        'I0 I1 Z2 Z3'
+                    ]
+    attribute_dictionary =  {
+                            'Cofactors': [(-0.32760818995565577+0j),
+                                          (0.1371657293179602+0j),
+                                          (0.1371657293179602+0j),
+                                          (-0.13036292044009176+0j),
+                                          (0.1632676867167479+0j)],
+                            'random_attribute': [0, 1, 2, 3, 4]
+                            }
+    L_nodes, node_attributes_dict = Get_list_of_nodes_and_attributes(List_of_nodes, attribute_dictionary)
 
-    node_index_set = [j for j in range(len(PauliWords))]
+    attrib_dict={}
+    for i in range(len(List_of_nodes)):
+        temp_dict={}
+        for key in attribute_dictionary:
+            temp_dict[key] = attribute_dictionary[key][i]
+        attrib_dict[List_of_nodes[i]] = temp_dict
 
-    assert(node_index_set == graph.node_index_set)
+    assert (List_of_nodes == L_nodes) and (attrib_dict == node_attributes_dict)
+
+
