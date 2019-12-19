ebcc00d6004cd48ee4f292c2f346ac65ae34b22d
AlexisRalli <44055008+AlexisRalli@users.noreply.github.com>
2019-12-19 16:00:40 +0000

    tree approach seems to be working. Have to think about after finding first tree if one wants to do further Unitary partitioning or whether to look for next best completely commuting tree. May be able to look at what is best via a numerics approach!

--- Projects/tree.py
+++ Projects/tree.py
@@ -67,12 +67,30 @@ m_colour = Colour_list_of_Graph(multi_G, attribute_dictionary=attribute_dictiona
 anti_commuting_sets = Get_unique_graph_colours(s_colour + m_colour)
 print(anti_commuting_sets)
 
-# anti_commuting_sets key ordering... smallest first!
-dict_ordering_list = sorted(anti_commuting_sets, key=lambda k: len(anti_commuting_sets[k]), reverse=False)
-node_list = list(G.nodes)
 
 
-def Commute(P1, P2):
+def Commutativity(P1, P2, anti_comm=False):
+    """
+     Find if two PauliWords either commute or anti_commute.
+     By default it will check if they commute.
+
+    Args:
+        P1 (str): First PauliWord to compare
+        P2 (str): Second PauliWord to compare
+
+    Returns:
+        (bool): True or false as to whether terms commute or anti_commute
+
+    .. code-block:: python
+       :emphasize-lines: 5
+
+       from TODO import *
+
+       P1 = 'I0 I1 I2 I3 I4 I5 Z6 I7 I8 I9 I10 I11'
+       P2 = 'I0 I1 Y2 I3 I4 I5 Z6 I7 I8 I9 X10 I11'
+       Commutativity(P1, P2, anti_comm=False)
+       >> True
+    """
     P1 = P1.split(' ')
     P2 = P2.split(' ')
 
@@ -86,47 +104,50 @@ def Commute(P1, P2):
             checker[i] = -1
 
     if reduce((lambda x, y: x * y), checker) == 1:
-        return True
+        if anti_comm is False:
+            return True
+        else:
+            return False
     else:
-        return False
+        if anti_comm is False:
+            return False
+        else:
+            return True
 
-# # print(Commute(str(*anti_commuting_sets[2][0].keys()), str(*anti_commuting_sets[2][1].keys())))
-# # print(Commute(str(*anti_commuting_sets[0][0].keys()), str(*anti_commuting_sets[2][1].keys())))
+# # print(Commutativity(str(*anti_commuting_sets[2][0].keys()), str(*anti_commuting_sets[2][1].keys())))
+# # print(Commutativity(str(*anti_commuting_sets[0][0].keys()), str(*anti_commuting_sets[2][1].keys())))
 #
-# H = G.copy()
-# #remove all of first set
-# H.remove_nodes_from([P_word for sets in anti_commuting_sets[0] for P_word in sets])
-#
-# #remove all of second set BAR first term
-# H.remove_nodes_from([P_word for sets in anti_commuting_sets[1][1::] for P_word in sets])
-#
-#
-#
-# # may be able to use max clique function (finds max no. of fully connected subgraphs!)
-# # iterate through tree, selecting one P_word from each row
-# # find max cliques
-# list(nx.clique.find_cliques(H))
-# min(list(nx.clique.find_cliques(H)))
 
 
-####################
-
-# # anti_commuting_sets key ordering... smallest first!
-# dict_ordering_list = sorted(anti_commuting_sets, key=lambda k: len(anti_commuting_sets[k]), reverse=False)
-#
-# # get term with largest number of P_words
-# max_num_terms = len(anti_commuting_sets[dict_ordering_list[-1]])
-
-# LONGEST ANTI_COMMUTING SET
 
 def Get_longest_anti_commuting_set(anti_commuting_sets):
-    # finds length of longest anti_commuting set!
+    """
+     Finds length of largest list in dictionary and returns it. In this case it will
+     return the length of the largest anti_commuting set.
+
+    Args:
+        anti_commuting_sets (dic): Dictionary of anti_commuting sets of PauliWords
+
+    Returns:
+        (int): Length of largest anti_commuting set
+    """
     max_key, max_value = max(anti_commuting_sets.items(), key=lambda x: len(x[1]))
     return len(max_value)
 
 def Make_anti_commuting_sets_same_length(anti_commuting_sets, max_set_size):
-    # goes through each term
-    # appending None if term has less than maximum number of terms
+    """
+     Makes every set in anti_commuting_sets dictionary the same length, by adding None terms
+     to instance when too short.
+
+     This is very useful for finding 'longest tree'
+
+    Args:
+        anti_commuting_sets (dic): Dictionary of anti_commuting sets of PauliWords
+        max_set_size (int): Length of longest anti_commuting set
+
+    Returns:
+        FILLED_anti_commuting_sets (dic): Dictionary of anti_commuting_sets with all sets the same size
+    """
 
     FILLED_anti_commuting_sets = {}
 
@@ -144,7 +165,7 @@ max_set_size = Get_longest_anti_commuting_set(anti_commuting_sets)
 FILLED_anti_commuting_sets = Make_anti_commuting_sets_same_length(anti_commuting_sets, max_set_size)
 
 
-def Find_Longest_tree(FILLED_anti_commuting_sets, max_set_size):
+def Find_Longest_tree(FILLED_anti_commuting_sets, max_set_size, anti_comm=False):
 
     tree = {}
     best_reduction_possible = len(FILLED_anti_commuting_sets)  # <-- aka need this many things in branch_instance!
@@ -176,7 +197,7 @@ def Find_Longest_tree(FILLED_anti_commuting_sets, max_set_size):
                             k_max -= 1
                             continue
                         else:
-                            if False not in [Commute(term, str(*P_comp.keys())) for term in branch_instance]:
+                            if False not in [Commutativity(term, str(*P_comp.keys()), anti_comm=anti_comm) for term in branch_instance]:
                                 # print(key, i, k, j)
                                 k_max -= 1
                                 jk_list.append((j, k))
@@ -207,7 +228,56 @@ def Find_Longest_tree(FILLED_anti_commuting_sets, max_set_size):
 
 # actually only need best combo output!
 # best combo defines which terms to make pauli S ! maximises commutativity!!!
-tree, best_combo = Find_Longest_tree(FILLED_anti_commuting_sets, max_set_size)
+tree_commute, best_combo_commute = Find_Longest_tree(FILLED_anti_commuting_sets, max_set_size, anti_comm= False)
+#tree_anti, best_combo_anti = Find_Longest_tree(FILLED_anti_commuting_sets, max_set_size, anti_comm= True)
+
 
 # want to maximise the anti_commutativity between the remaining terms to do further Unitary partitioning!
-missing_k = [k for k in anti_commuting_sets.keys() if k not in [key for index, key in best_combo['j_k']]]
+missing_k = [k for k in anti_commuting_sets.keys() if k not in [key for index, key in best_combo_commute['j_k']]]
+
+# only look at missing keys!!!
+new_anti_commuting_sets={}
+i=0
+for key in missing_k:
+    # new_anti_commuting_sets[key]=anti_commuting_sets[key]
+    new_anti_commuting_sets[i] = anti_commuting_sets[key]
+    i+=1
+
+max_set_size = Get_longest_anti_commuting_set(new_anti_commuting_sets)
+NEW_FILLED_anti_commuting_sets = Make_anti_commuting_sets_same_length(new_anti_commuting_sets, max_set_size)
+
+# HERE can either look for next best commutative tree OR look for best anti_commuting terms to do further UP too!
+tree_anti, best_combo_anti = Find_Longest_tree(NEW_FILLED_anti_commuting_sets, max_set_size, anti_comm= True)
+
+print(best_combo_anti)
+
+# old idea (not working)
+# # anti_commuting_sets key ordering... smallest first!
+# dict_ordering_list = sorted(anti_commuting_sets, key=lambda k: len(anti_commuting_sets[k]), reverse=False)
+# node_list = list(G.nodes)
+
+# H = G.copy()
+# #remove all of first set
+# H.remove_nodes_from([P_word for sets in anti_commuting_sets[0] for P_word in sets])
+#
+# #remove all of second set BAR first term
+# H.remove_nodes_from([P_word for sets in anti_commuting_sets[1][1::] for P_word in sets])
+#
+#
+#
+# # may be able to use max clique function (finds max no. of fully connected subgraphs!)
+# # iterate through tree, selecting one P_word from each row
+# # find max cliques
+# list(nx.clique.find_cliques(H))
+# min(list(nx.clique.find_cliques(H)))
+
+
+####################
+
+# # anti_commuting_sets key ordering... smallest first!
+# dict_ordering_list = sorted(anti_commuting_sets, key=lambda k: len(anti_commuting_sets[k]), reverse=False)
+#
+# # get term with largest number of P_words
+# max_num_terms = len(anti_commuting_sets[dict_ordering_list[-1]])
+
+# LONGEST ANTI_COMMUTING SET
