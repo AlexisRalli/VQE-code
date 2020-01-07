240e37802500a351a2fa5b99d756cad38a5caf68
AlexisRalli <rallilex@hotmail.co.uk>
2020-01-07 18:06:49 +0000

    error in Find_longest_tree function. Need to think about how to correctly implement alg!

--- Projects/PauilS_Selection.py
+++ Projects/PauilS_Selection.py
@@ -48,90 +48,77 @@ anti_commuting_sets = Get_unique_graph_colours(s_colour + m_colour)
 ######################
 
 
-S_dict, QWC_terms_to_measure = Get_all_S_terms(anti_commuting_sets, 'QWC')
+#S_dict, QWC_terms_to_measure = Get_all_S_terms(anti_commuting_sets, 'QWC')
 
 
-# # putting anti_commuting_set dict in correct format for function!!! #TODO need to change original function! this is temporary fix!
-di = {}
-for key in anti_commuting_sets:
-    temp_l = []
-    for DICT in anti_commuting_sets[key]:
-        for P_Word in DICT:
-            temp_l.append((P_Word, DICT[P_Word]['Cofactors']))
-        di[key] = temp_l
+# error for ANTI COMMUTING EXAMPLE!!!
+S_dict, QWC_terms_to_measure = Get_all_S_terms(anti_commuting_sets, 'AC')
+print(reduce(lambda count, LIST: count + len(LIST), QWC_terms_to_measure, 0))
+QWC_terms_to_measure[-6]
 
+# error in Find_Longest_tree
 
-### Ansatz
-from quchem.Ansatz_Generator_Functions import *
-qubits = cirq.LineQubit.range(Hamilt.MolecularHamiltonian.n_qubits)
-full_anstaz_circuit = cirq.Circuit.from_ops(cirq.X(qubits[0]), cirq.X(qubits[1]))
-# HF_initial_state= HF_state_generator(n_electrons, Hamilt.MolecularHamiltonian.n_qubits)
-# HF_UCC = Full_state_prep_circuit(HF_initial_state, T1_and_T2_theta_list=[])
-# HF_UCC.complete_UCC_circuit()
-# full_anstaz_circuit =HF_UCC.UCC_full_circuit
-
-
-zz = UnitaryPartition(di, full_anstaz_circuit, S_dict=S_dict)
-zz.Get_Quantum_circuits_and_constants()
-circuits_and_constants = zz.circuits_and_constants
-print('NUMBER of Q circuits = ',len(circuits_and_constants))
-print('Only need to perform = ', len(QWC_terms_to_measure), 'of them!')
-print('can measure the rest due to QWC')
-
-# these terms should QWC commute(85, 0), (92, 5)
-P1=circuits_and_constants[85]['PauliWord']
-P2=circuits_and_constants[92]['PauliWord']
-print(Commutativity(P1, P2, 'QWC'))
-
-
-PP1 = list(anti_commuting_sets[85][0].keys())[0]
-PP2 = list(anti_commuting_sets[92][5].keys())[0]
-print(Commutativity(PP1, PP2, 'QWC'))
-
-
-
-
-
-
-
-
-
-# NEW_anti_commuting_sets = {}
-# for key in S_dict:
-#     NEW_anti_commuting_sets[key] = anti_commuting_sets[key][S_dict[key]]
-#
-# List_PauliWords=[]
-# HamiltonainCofactors=[]
-#
-# for key in S_dict:
-#     for P_Word, DICT in NEW_anti_commuting_sets[key].items():
-#         List_PauliWords.append(P_Word)
-#         HamiltonainCofactors.append(DICT['Cofactors'])
+# def Find_Longest_tree(FILLED_anti_commuting_sets, max_set_size, anti_comm):
+#     """
+#      From a dictionary of fully anti-commuting sets (each entry is of same length, None used as filler. Finds
+#      longests possible either fully anti-commuting or fully commuting subsets between rows #TODO need to find QWC too!
 #
 #
-# List_of_nodes = List_PauliWords
-# attribute_dictionary = {'Cofactors': HamiltonainCofactors}
+#     Args:
+#         FILLED_anti_commuting_sets (dict): Dictionary of anti_commuting sets of PauliWords
+#         max_set_size (int): Length of longest anti_commuting set
+#         anti_comm_QWC (str): flags to find either:
+#                                            qubit wise commuting (QWC) terms  -> flag = 'QWC',
+#                                                              commuting terms -> flag = 'C',
+#                                                         anti-commuting terms -> flag = 'AC'
 #
-# List_of_nodes, node_attributes_dict = Get_list_of_nodes_and_attributes(List_of_nodes,
-#                                                                        attribute_dictionary=attribute_dictionary)
+#     Returns:
+#         tree (dict): Dictionary of selected PauliWord: i_key = (index, row) and j_k = list of (index, row) either
+#                     commuting or anti-commuting PauliWords (list)
+#         best_combo (dict): selected PauliWord: i_key = (index, row) and j_k = list of (index, row) of best possible
+#                           sub set
 #
-# G = nx.Graph()
-# G = Build_Graph_Nodes(List_of_nodes, G, node_attributes_dict=node_attributes_dict, plot_graph=False)
-# G = Build_Graph_Edges_COMMUTING_QWC_AntiCommuting(G, List_of_nodes,'C', plot_graph = False)
+#     """
+#     tree = {}
+#     key_list = list(FILLED_anti_commuting_sets.keys())
 #
-# # comp_G = Get_Complemenary_Graph(G, node_attributes_dict=node_attributes_dict, plot_graph=True) # <- not currently used
+#     # for key in FILLED_anti_commuting_sets:
+#     for INDEX in tqdm(range(len(FILLED_anti_commuting_sets)), ascii=True, desc='Getting best Branch'):
+#
+#         running_best = 0
+#         best_reduction_possible = len(key_list[INDEX:])  # <-- aka how many fully anti-commuting sets
+#
+#         key = key_list[INDEX]
+#         selected_set = FILLED_anti_commuting_sets[key]
+#         full_branch_key = []
+#
+#         for i in range(len(selected_set)):
+#             P_word = selected_set[i]  # this will be the 'top' of the tree
+#             branch_instance = []
+#             branch_instance_holder = {}
+#
+#             if P_word is None:
+#                 continue
+#             else:
+#                 branch_instance.append(str(*P_word.keys()))
+#                 jk_list = []
+#
+#                 max_k = len(key_list[INDEX + 1:])  # remaining rows!
+#
+#                 for j in range(max_set_size):
+#                     for k in key_list[INDEX + 1:][:-2]:
+#                         for jj in range(max_set_size):
+#                             P_comp = FILLED_anti_commuting_sets[k + 1][jj]
+#
+#                             print(k, jj)
+
+#####
+print(blah)
+###
+
 #
 #
-# single_G, multi_G = Get_subgraphs(G, node_attributes_dict=node_attributes_dict)
-# s_colour = Colour_list_of_Graph(single_G, attribute_dictionary=attribute_dictionary, plot_graph=False,
-#                                 strategy='largest_first')
-# m_colour = Colour_list_of_Graph(multi_G, attribute_dictionary=attribute_dictionary, plot_graph=False,
-#                                 strategy='largest_first')
-#
-# anti_commuting_sets = Get_unique_graph_colours(s_colour + m_colour)
-#
-#
-# # putting anti_commuting_set dict in correct format for function!!! #TODO need to change original function! this is temporary fix!
+# # # putting anti_commuting_set dict in correct format for function!!! #TODO need to change original function! this is temporary fix!
 # di = {}
 # for key in anti_commuting_sets:
 #     temp_l = []
@@ -139,7 +126,6 @@ print(Commutativity(PP1, PP2, 'QWC'))
 #         for P_Word in DICT:
 #             temp_l.append((P_Word, DICT[P_Word]['Cofactors']))
 #         di[key] = temp_l
-#
 #
 #
 # ### Ansatz
@@ -152,8 +138,21 @@ print(Commutativity(PP1, PP2, 'QWC'))
 # # full_anstaz_circuit =HF_UCC.UCC_full_circuit
 #
 #
-# zz = UnitaryPartition(di, full_anstaz_circuit, S=S_dict)
+# zz = UnitaryPartition(di, full_anstaz_circuit, S_dict=S_dict)
 # zz.Get_Quantum_circuits_and_constants()
 # circuits_and_constants = zz.circuits_and_constants
 # print('NUMBER of Q circuits = ',len(circuits_and_constants))
-# print('Only need to perform = ', len(QWC_terms_to_measure))
+# print('Only need to perform = ', len(QWC_terms_to_measure), 'of them!')
+# print('can measure the rest due to QWC')
+#
+# # these terms should QWC commute(85, 0), (92, 5)
+# P1=circuits_and_constants[85]['PauliWord']
+# P2=circuits_and_constants[92]['PauliWord']
+# print(Commutativity(P1, P2, 'QWC'))
+#
+#
+# PP1 = list(anti_commuting_sets[85][0].keys())[0]
+# PP2 = list(anti_commuting_sets[92][5].keys())[0]
+# print(Commutativity(PP1, PP2, 'QWC'))
+#
+
