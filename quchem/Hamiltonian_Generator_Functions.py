5cbcd10a6c7cea86ac43b66e4684aebcc8d60fc0
AlexisRalli <rallilex@hotmail.co.uk>
2020-01-09 20:38:37 +0000

    documentation added

--- quchem/Hamiltonian_Generator_Functions.py
+++ quchem/Hamiltonian_Generator_Functions.py
@@ -12,17 +12,24 @@ class Hamiltonian():
 
     Args:
         MoleculeName (str): Name of Molecule
-        run_scf (int, optional): Bool to run
-        run_mp2 (int, optional):
-        run_cisd (int, optional):
-        run_ccsd (int, optional):
-        run_fci (int, optional):
-        basis (int, optional):
-        multiplicity (int, optional):
-        geometry (int, optional):
+        run_scf (int): Boolean to run SCF calculation.
+        run_mp2 (int): Boolean to run MP2 calculation.
+        run_cisd (int): Boolean to run CISD calculation.
+        run_ccsd (int): Boolean to run CCSD calculation.
+        run_fci (int): Boolean to FCI calculation.
+        multiplicity_(int): Multiplicity of molecule (1=singlet, 2=doublet ... etc)
+        geometry (list, optional): Geometry of Molecule (if None will find it online)
 
     Attributes:
-        #TODO
+        molecule (openfermion.hamiltonians._molecular_data.MolecularData): An instance of the MolecularData class
+
+        molecule.single_cc_amplitudes (numpy.array): h_pq (n_qubits x n_qubits) numpy array
+        molecule.double_cc_amplitudes (numpy.array): h_pqrs (n_qubits x n_qubits x n_qubits x n_qubits) numpy array
+
+        singles_hamiltonian (numpy.ndarray): h_pq (n_qubits x n_qubits) matrix.
+        doubles_hamiltonian (numpy.ndarray): h_pqrs (n_qubits x n_qubits x n_qubits x n_qubits) matrix
+        MolecularHamiltonian (openfermion.ops._interaction_operator.InteractionOperator):
+        MolecularHamiltonianMatrix (scipy.sparse.csc.csc_matrix): Sparse matrix of Molecular Hamiltonian
 
     """
     def __init__(self, MoleculeName,
@@ -43,6 +50,7 @@ class Hamiltonian():
         self.molecule = None
 
 
+
     def Run_Psi4(self):
 
         if self.geometry is None:
@@ -59,7 +67,7 @@ class Hamiltonian():
         self.molecule.filename = self.MoleculeName
 
         # Run Psi4.
-        self.molecule_Psi4 = run_psi4(self.molecule,
+        self.molecule = run_psi4(self.molecule,
                             run_scf=self.run_scf,
                             run_mp2=self.run_mp2,
                             run_cisd=self.run_cisd,
@@ -77,6 +85,14 @@ class Hamiltonian():
 
 
     def Get_CCSD_Amplitudes(self):
+        """
+        Parse coupled cluster singles and doubles amplitudes from psi4 file
+        where: H = constant + ∑_pq (h_pq a†_p a_q) + ∑_pqrs (h_pqrs a†_p a†_q a_r a_s)
+        note that:
+            - single amplitudes: h_pq is a (n_qubits x n_qubits) numpy array
+            - doubles amplitudes: h_pqrs is a (n_qubits x n_qubits x n_qubits x n_qubits) numpy array
+        """
+
         from openfermionpsi4._psi4_conversion_functions import parse_psi4_ccsd_amplitudes
         # https://github.com/quantumlib/OpenFermion-Psi4/blob/master/openfermionpsi4/_psi4_conversion_functions.py
         self.molecule.single_cc_amplitudes, self.molecule.double_cc_amplitudes = (
@@ -85,6 +101,7 @@ class Hamiltonian():
                                                                         self.molecule.get_n_alpha_electrons(),
                                                                         self.molecule.get_n_beta_electrons(),
                                                                         self.molecule.filename + ".out"))
+
     def PrintInfo(self):
         if self.molecule is None:
             self.Run_Psi4()
@@ -99,6 +116,17 @@ class Hamiltonian():
         print('FCI: ', self.molecule.fci_energy)
 
     def Get_Molecular_Hamiltonian(self):
+        """
+        Get sparse matrix of molecular Hamiltonian
+
+        Attributes:
+            singles_hamiltonian (numpy.ndarray): h_pq (n_qubits x n_qubits) matrix.
+            doubles_hamiltonian (numpy.ndarray): h_pqrs (n_qubits x n_qubits x n_qubits x n_qubits) matrix
+            MolecularHamiltonian (openfermion.ops._interaction_operator.InteractionOperator):
+            MolecularHamiltonianMatrix (scipy.sparse.csc.csc_matrix): Sparse matrix of Molecular Hamiltonian
+
+
+        """
         if self.molecule is None:
             self.Run_Psi4()
 
@@ -113,23 +141,26 @@ class Hamiltonian():
         self.MolecularHamiltonianMatrix = get_sparse_operator(self.MolecularHamiltonian)
 
 
-        # Get Fermionic Hamiltonian
-        from openfermion.transforms import get_fermion_operator
-        fermionic_hamiltonian = get_fermion_operator(self.MolecularHamiltonian)
-
-        # get Qubit Hamiltonian
-        from openfermion.transforms import jordan_wigner
-        self.QubitHamiltonian = jordan_wigner(fermionic_hamiltonian)
-
-
-
     def  Get_Basis_state_in_occ_num_basis(self, occupied_orbitals_index_list=None):
-        #Function to produce a basis state in the occupation number basis.
+        """
 
-        #  aka input normally occupied_orbitals_index_list of HF state... eg |HF> = |0011>  --> INPUT = [0,1]
-        # output is |HF> and <HF| state vectors under JW transform!
+        Method to obtain basis state under JW transform of state defined in occupation number basis.
+        e.g. for H2 under the Jordan Wigner transfrom has |HF> = |0011> in occ no. basis
+        occupied_orbitals_index_list = [0,1] <- as first orbitals occupied
 
-        import scipy
+        These outputs (|HF> and <HF|) can be used with MolecularHamiltonianMatrix!.
+
+        Args:
+            occupied_orbitals_index_list (list, optional): list of orbital indices that are OCCUPIED
+
+        returns:
+            reference_ket (scipy.sparse.csr.csr_matrix): Sparse matrix of KET corresponding to occ no basis state under
+                                                         JW transform
+            reference_bra (scipy.sparse.csr.csr_matrix): Sparse matrix of BRA corresponding to occ no basis state under
+                                                         JW transform
+
+        """
+
         from openfermion import jw_configuration_state
 
         if occupied_orbitals_index_list is None:
@@ -145,15 +176,33 @@ class Hamiltonian():
         # hamiltonian_ket = self.MolecularHamiltonianMatrix.dot(reference_ket)
 
     def Get_ia_and_ijab_terms(self, Coupled_cluser_param=False, filter_small_terms = False): #TODO could add MP2 param option to initialise theta with MP2 amplitudes (rather than coupled cluster only option)
+        """
 
-        # Sec_Quant_CC_ops is a list of fermionic creation and annihilation operators that perform UCCSD
+        Get ia and ijab terms as fermionic creation and annihilation operators which perform UCCSD.
+        These ia and ijab terms are standard single and double excitation terms (aka only occupied -> unoccupied transitions allowed)
+        (faster and only marginally less accurate.)
+        #TODO can add method to get pqrs terms
+        #TODO these are all non-degenerate excitations which can possibly non-zero, including nocc->nocc, occ->occ, and spin-flips.
+        #TODO EXPENSIVE, but will likely  get a slightly better answer.
 
-        # e.g. for H2:
-        #  Sec_Quant_CC_ops=  [
-        #                      -1.0[0 ^ 2] + 1.0[2 ^ 0],            # -(a†0 a2) + (a†2 a0)
-        #                      -1.0[1 ^ 3] + 1.0[3 ^ 1],            # -(a†1 a3) + (a†3 a1)
-        #                      -1.0[0 ^ 1 ^ 2 3] + 1.0 [3^ 2^ 1 0]  # -(a†0 a†1 a2 a3) + a†3 a†2 a1 a0)
-        #                     ]
+        Args:
+            Coupled_cluser_param (bool, optional): Whether to use CC calculated amplitdues to initialise theta_angles
+            filter_small_terms (bool, optional):  Whether to filter small terms in Hamiltonian (threshold currently hardcoded)
+
+        returns:
+            Sec_Quant_CC_ops (list): list of FermionOperators (openfermion.ops._fermion_operator.FermionOperator)
+            theta_parameters (list): list of theta values (parameterisation of excitation amplitudes)
+
+         e.g. for H2:
+         Sec_Quant_CC_ops=  [
+                             -1.0[0 ^ 2] + 1.0[2 ^ 0],            # -(a†0 a2) + (a†2 a0)
+                             -1.0[1 ^ 3] + 1.0[3 ^ 1],            # -(a†1 a3) + (a†3 a1)
+                             -1.0[0 ^ 1 ^ 2 3] + 1.0 [3^ 2^ 1 0]  # -(a†0 a†1 a2 a3) + a†3 a†2 a1 a0)
+                            ]
+        theta_parameters = [0,0,0]
+
+        """
+
 
         if self.molecule is None:
             self.Run_Psi4()
@@ -307,24 +356,77 @@ class Hamiltonian():
         return Sec_Quant_CC_ops, theta_parameters
 
 class Hamiltonian_Transforms():
-    def __init__(self, MolecularHamiltonian, Sec_Quant_CC_ops):
+    """
+
+    The Hamiltonian_Transforms object manipulates outputs from Hamiltonian class
+    Can get:
+        - Fermionic Molecular Hamiltonian
+        - Qubit Molecular Hamiltonian under JW transform
+        - Jordan Wigner CC matrices
+
+    Args:
+        MolecularHamiltonian (openfermion.ops._interaction_operator.InteractionOperator):
+        Sec_Quant_CC_ops (list): list of FermionOperators (openfermion.ops._fermion_operator.FermionOperator)
+        n_qubits (int): Number of qubits
+
+    """
+    def __init__(self, MolecularHamiltonian, Sec_Quant_CC_ops, n_qubits):
         self.MolecularHamiltonian = MolecularHamiltonian
         self.Sec_Quant_CC_ops = Sec_Quant_CC_ops
+        self.n_qubits = n_qubits
 
     def Get_Fermionic_Hamiltonian(self):
-        #  Gives second quantised Hamiltonian
-        # H = h00 a†0a0 + h11a†1a1 + h22a†2a2 +h33a†3a3 +
-        #     h0110 a†0a†1a1a0 +h2332a†2a†3a3a2 + ... etc etc
 
-        # note can get integrals from Get_CCSD_Amplitudes method of Hamiltonian class!
+        """
+        Returns the second quantised Fermionic Molecular Hamiltonian of the Molecular Hamiltonian
+
+        e.g. H = h00 a†0a0 + h11a†1a1 + h22a†2a2 + h33a†3a3 + h0110 a†0a†1a1a0 +h2332a†2a†3a3a2 + ... etc etc
+
+        note can get integrals (h_ia and h_ijab) from Get_CCSD_Amplitudes method of Hamiltonian class!
+
+
+        returns:
+            FermionicHamiltonian (openfermion.ops._fermion_operator.FermionOperator): Fermionic Operator
+
+        e.g. for H2:
+                             0.7151043387432434 [] +
+                            -1.2533097864345657 [0^ 0] +
+                            0.3373779633738658 [0^ 0^ 0 0] +
+                            0.09060523101737843 [0^ 0^ 2 2] +
+                            0.3373779633738658 [0^ 1^ 1 0] +
+                            0.09060523101737843 [0^ 1^ 3 2] + ... etc ... etc
+
+        """
+
 
         from openfermion.transforms import get_fermion_operator
         FermionicHamiltonian = get_fermion_operator(self.MolecularHamiltonian)
         return FermionicHamiltonian
 
     def Get_Qubit_Hamiltonian_JW(self):
-        #  Gives second quantised Hamiltonian under the JW transform!
-        # H = h0 I + h1 Z0 + h2 Z1 +h3 Z2 + h4 Z3 + h5 Z0Z1 ... etc etc
+
+        """
+        Returns the second quantised Qubit Molecular Hamiltonian of the Molecular Hamiltonian using the
+        JORDAN WIGNER transformation. First gets the fermionic Hamiltonian and than performs JW.
+
+        e.g. H = h0 I + h1 Z0 + h2 Z1 +h3 Z2 + h4 Z3 + h5 Z0Z1 ... etc etc
+        note can get integrals (h_ia and h_ijab) from Get_CCSD_Amplitudes method of Hamiltonian class!
+
+
+
+        returns:
+            QubitHamiltonian (openfermion.ops._qubit_operator.QubitOperator): Qubit Operator
+
+        e.g. for H2:
+                             (-0.09706626861762624+0j) [] +
+                            (-0.04530261550868928+0j) [X0 X1 Y2 Y3] +
+                            (0.04530261550868928+0j) [X0 Y1 Y2 X3] +
+                            (0.04530261550868928+0j) [Y0 X1 X2 Y3] +
+                            (-0.04530261550868928+0j) [Y0 Y1 X2 X3] +
+                            (0.17141282639402405+0j) [Z0] + ... etc etc
+
+        """
+
 
         from openfermion.transforms import jordan_wigner
         FermionicHamiltonian = self.Get_Fermionic_Hamiltonian()
@@ -332,6 +434,36 @@ class Hamiltonian_Transforms():
         return QubitHamiltonian
 
     def Get_Jordan_Wigner_CC_Matrices(self):
+        """
+        From list of FermionOperators (openfermion.ops._fermion_operator.FermionOperator) corresponding to
+        UCCSD excitations... remember: UCCSD = ∑_pq (h_pq a†_p a_q) + ∑_pqrs (h_pqrs a†_p a†_q a_r a_s)
+
+                    [
+                       -(a†0 a2) + (a†2 a0),
+                       -(a†1 a3) + (a†3 a1),
+                       -(a†0 a†1 a2 a3) + a†3 a†2 a1 a0)
+                    ]
+
+        calculates corresponding sparse matrices for each term under the JORDAN WIGNER transform
+        and returns them in a list. AKA returns list of matrices corresponding to each UCCSD excitation operator
+
+        e.g. H = h0 I + h1 Z0 + h2 Z1 +h3 Z2 + h4 Z3 + h5 Z0Z1 ... etc etc
+        note can get integrals (h_ia and h_ijab) from Get_CCSD_Amplitudes method of Hamiltonian class!
+
+        e.g. output
+        [
+            <16x16 sparse matrix of type '<class 'numpy.complex128'>',
+            <16x16 sparse matrix of type '<class 'numpy.complex128'>',
+            <16x16 sparse matrix of type '<class 'numpy.complex128'>'
+        ]
+
+
+        returns:
+            JW_CC_ops (list): List of Sparse matrices corresponding to each UCCSD excitation operator
+
+
+        """
+
         # converts list of UCCSD fermionic operations from:  ∑_pq (h_pq a†_p a_q) + ∑_pqrs (h_pqrs a†_p a†_q a_r a_s)
         # list of matrix operations to perform each fermionic operation (under JW transform).
 
@@ -347,10 +479,21 @@ class Hamiltonian_Transforms():
         JW_CC_ops = []
         for classical_op in self.Sec_Quant_CC_ops:
             # matrix operator of coupled cluster operations
-            JW_CC_ops.append(get_sparse_operator(classical_op, n_qubits=4))
+            JW_CC_ops.append(get_sparse_operator(classical_op, n_qubits=self.n_qubits))
         return JW_CC_ops
 
 class CalcEnergy():
+    """
+    The CalcEnergy object calculates Energies using LINEAR ALG.
+
+    Args:
+        MolecularHamiltonianMatrix (scipy.sparse.csc.csc_matrix): Sparse matrix of Molecular Hamiltonian
+        n_qubits (int): Number of qubits
+        JW_CC_ops (list): List of Sparse matrices corresponding to each UCCSD excitation operator
+        reference_ket (scipy.sparse.csr.csr_matrix): Sparse matrix of KET corresponding to occ no basis state under
+                                                     JW transform (see Get_Basis_state_in_occ_num_basis function
+                                                     of Hamiltonian class)
+    """
     def __init__(self, MolecularHamiltonianMatrix, reference_ket, n_qubits, JW_CC_ops_list):
         self.MolecularHamiltonianMatrix = MolecularHamiltonianMatrix
         self.reference_ket = reference_ket
@@ -360,13 +503,35 @@ class CalcEnergy():
 
 
     def Calc_HF_Energy(self):
+        """
+        Returns Hartree-Fock Energy calculated by multiplying HF state with Molecular Hamiltonian matrix.
+
+        aka H |HF> = E_hartree |HF>
+
+        returns:
+            HF_energy (float): Hartree Fock Energy
+        """
+
         HF_ket = self.MolecularHamiltonianMatrix.dot(self.reference_ket).toarray()  # H |HF_ref> =   E*|HF> (all in one vecotr)
         HF_energy = np.dot(HF_ref_bra.toarray(), HF_ket)  # selects correct entries as in vector giving E (aka uses E |state vec>)
         print('HF Energy from lin alg: ', HF_energy)
         return HF_energy
 
     def Calc_UCCSD_No_Trot(self, parameters):
-        # apply UCCSD matrix WITHOUT trotterisation!
+        """
+        Returns UCCSD Energy calculated by applying UCCSD exponentiated matrix to HF state.
+        NO TROTTERISATION PERFORMED!!!
+
+        e.g. for H2 UCCSD_OP = exp [ t02(a†2a0−a†0a2) + t13(a†3a1−a†1a3) + t0123(a†3a†2a1a0−a†0a†1a2a3) ]
+
+        aka:
+            UCCSD_OP |HF> = |UCCSD>
+        THEN:
+            H |UCCSD> =  E_UCCSD |UCCSD>
+
+        returns:
+            UCCSD Energy (No trotterisation)
+        """
 
         generator = scipy.sparse.csc_matrix((2 ** (self.n_qubits), 2 ** (self.n_qubits)), dtype=complex)
         for mat_op in range(0, len(self.JW_CC_ops_list)):
@@ -379,7 +544,29 @@ class CalcEnergy():
         return energy.toarray()[0][0].real
 
     def Calc_UCCSD_with_Trot(self, parameters):
-        # apply UCCSD matrix WITH first order trotterisation!
+
+        """
+        Returns UCCSD Energy calculated by applying FIRST ORDER UCCSD operators to HF state.
+
+        e.g.
+            for H2
+
+            UCCSD_OP = exp [ t02(a†2a0−a†0a2) + t13(a†3a1−a†1a3) + t0123(a†3a†2a1a0−a†0a†1a2a3) ]
+
+            becomes
+
+            UCCSD_OP^TROTTER =exp [t02(a†2a0−a†0a2)] × exp [t13(a†3a1−a†1a3)] × exp [t0123(a†3a†2a1a0−a†0a†1a2a3)]
+
+         Function recersively applies UCCSD_OP^TROTTER to reference state (at beginning will be |HF> state)
+         yielding |UCCSD_trotter> state
+
+         finally performs
+            H |UCCSD_trotter> =  E_UCCSD_trotter |UCCSD_trotter>
+
+        returns:
+            UCCSD Energy (calculated WITH trotterisation)
+        """
+
 
         new_state = self.reference_ket
         for k in reversed(range(0, len(parameters))):
@@ -411,7 +598,7 @@ if __name__ == '__main__':
     SQ_CC_ops, THETA_params = Hamilt.Get_ia_and_ijab_terms(Coupled_cluser_param=True)
     print(SQ_CC_ops)
 
-    HF_transformations = Hamiltonian_Transforms(Hamilt.MolecularHamiltonian, SQ_CC_ops)
+    HF_transformations = Hamiltonian_Transforms(Hamilt.MolecularHamiltonian, SQ_CC_ops, Hamilt.molecule.n_qubits)
 
     QubitHam = HF_transformations.Get_Qubit_Hamiltonian_JW()
     print('Qubit Hamiltonian: ', QubitHam)
@@ -433,7 +620,6 @@ if __name__ == '__main__':
     THETA_params = [2.8, 2.1, 1]
 
     w.Calc_UCCSD_No_Trot(THETA_params)
-    w.Calc_UCCSD_with_Trot(THETA_params)
     w.Calc_UCCSD_with_Trot(THETA_params)
 
 
