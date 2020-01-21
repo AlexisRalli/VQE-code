595756f70d0bdb6b76fc0b23f8a682d54aca14ea
AlexisRalli <rallilex@hotmail.co.uk>
2020-01-21 09:49:14 +0000

    jupyter notebook notes on ALCU

--- tests/test_Hamiltonian_Generator_Functions.py
+++ tests/test_Hamiltonian_Generator_Functions.py
@@ -6,16 +6,41 @@ from scipy.sparse import kron
 
 import pytest
 
+molecule = 'H2'
+Hamilt = Hamiltonian(molecule,
+                     run_scf=1, run_mp2=1, run_cisd=1, run_ccsd=1, run_fci=1,
+                     basis='sto-3g',
+                     multiplicity=1,
+                     geometry=None)
 
+true_geometry = [('H', (2, 0, 0)), ('H', (3, 0, 0))]
 
-def test_geometry():
+def test_geometry_standard():
     """
     Standard use test
     """
 
-    X = Hamiltonian('H2')
+    Hamilt.Get_Geometry()
 
-    pass
+    assert Hamilt.geometry == true_geometry
+
+def test_Get_Geometry_WRONG_NAME():
+    mol_name = 'NOT a molecule'
+    H_test = Hamiltonian(mol_name,
+                         run_scf=1, run_mp2=1, run_cisd=1, run_ccsd=1, run_fci=1,
+                         basis='sto-3g',
+                         multiplicity=1,
+                         geometry=None)
+
+    with pytest.raises(ValueError) as excinfo:
+        assert H_test.Get_Geometry() in excinfo.value
+
+def test_Run_Psi4():
+    """
+    Standard use test
+    """
+    Hamilt.Run_Psi4()
+    assert Hamilt.molecule is not None
 
 def test_Get_Basis_state_in_occ_num_basis():
     # |f_(N −1) , ..., f_1 , f_0> →→→ |q_(N −1) , ..., q_1 , q_0>
@@ -25,35 +50,36 @@ def test_Get_Basis_state_in_occ_num_basis():
     # n_qubits = 4
     # jw_configuration_state(occupied_orbitals, n_qubits)
 
-    occupied_orbitals_index_list = [0, 1]
-    molecule = 'H2'
-    HF_state = [0, 0, 1, 1]
+    occupied_orbitals_index_list = [i for i in range(Hamilt.molecule.n_electrons)]
+    HF_state = [1 for _ in range(Hamilt.molecule.n_electrons)] + \
+               [0 for _ in range(Hamilt.molecule.n_qubits-Hamilt.molecule.n_electrons)] # note ORDER (occupied first)
 
     zero = bsr_matrix(np.array([[1],
-                             [0]]))
-
+                                [0]]))
     one = bsr_matrix(np.array([[0],
-                             [1]]))
+                               [1]]))
 
     OperatorsKeys = {
         '0': zero,
         '1': one,
     }
+    HF_state = [OperatorsKeys[str(i)] for i in HF_state] # note reverse order!
 
-
-    HF_state = [OperatorsKeys[str(i)] for i in HF_state[::-1]] # note reverse order!
 
     tensored_ket = reduce(kron, HF_state)
     tensored_bra = np.transpose(tensored_ket)
 
-    Hamilt = Hamiltonian(molecule,
-                         run_scf=1, run_mp2=1, run_cisd=1, run_ccsd=1, run_fci=1,
-                         basis='sto-3g',
-                         multiplicity=1,
-                         geometry=None)
     Hamilt.Get_Molecular_Hamiltonian()
     ket, bra = Hamilt.Get_Basis_state_in_occ_num_basis(occupied_orbitals_index_list)
 
-    assert np.array_equal(ket.todense(), tensored_ket.todense()) and np.array_equal(bra.todense(), tensored_bra.todense())
+    assert np.array_equal(ket.todense(), tensored_ket.todense()) and \
+           np.array_equal(bra.todense(), tensored_bra.todense())
 
 
+#TODO
+# def test_Get_CCSD_Amplitudes()
+
+def test_Get_Molecular_Hamiltonian():
+    Hamilt.Get_Molecular_Hamiltonian()
+
+    mol_ham_mat = Hamilt.MolecularHamiltonianMatrix
