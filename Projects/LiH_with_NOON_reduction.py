Have fixed bug in X_set and R_LCU normalization! NEXT need to look at NOON method... 1. When keeping things in orbitals that are NOT present in HF state... need to add X gate

Projects/LiH_with_NOON_reduction.py
@@ -74,16 +74,16 @@ ansatz_Q_cicuit = HF_UCCSD_ansatz.Get_Full_HF_UCCSD_QC(THETA_params_REDUCED)
 #     print(results.histogram(key='0,1,2,3,4,5,6,7,8,9,10,11'))  # Need key to match number of qubits!!!
 
 ### Join Circuits
-LCU_Dict = Get_R_linear_combination(anti_commuting_set_stripped[9], 0, Hamilt.molecule.n_qubits)
-number_ancilla_qubits = int(np.ceil(np.log2(len(LCU_Dict['R_LCU']))))
 
-#TODO bug noticed... number of ancilla qubits changes per LCU dict... NEED TO FIX this!
-# currently loop to find max!
-a_list=[]
-for i in range(len(anti_commuting_set_stripped)):
-    LCU_Dict = Get_R_linear_combination(anti_commuting_set_stripped[i], 0, Hamilt.molecule.n_qubits)
-    a_list.append(int(np.ceil(np.log2(len(LCU_Dict['R_LCU'])))))
-number_ancilla_qubits = max(a_list)
+# #TODO bug noticed... number of ancilla qubits changes per LCU dict... NEED TO FIX this!
+# # currently loop to find max!
+# a_list=[]
+# for i in range(len(anti_commuting_set_stripped)):
+#     if len(anti_commuting_set_stripped[i])>1:
+#         print(i)
+#         LCU_Dict = Get_R_linear_combination(anti_commuting_set_stripped[i], 0, Hamilt.molecule.n_qubits)
+#         a_list.append(int(np.ceil(np.log2(len(LCU_Dict['R_LCU'])))))
+# number_ancilla_qubits = max(a_list)
 
 S_dict = {i:0 for i in range(len(anti_commuting_set_stripped))}
 w=ALCU_dict(ansatz_Q_cicuit, anti_commuting_set_stripped, S_dict, Hamilt.molecule.n_qubits)

quchem/LCU_method.py
@@ -384,7 +384,9 @@ def Get_R_linear_combination(anti_commuting_set, S_index, no_qubits):
     seperator = ' '
     I_term = seperator.join(['I{}'.format(i) for i in range(no_qubits)])
 
-    phi_n_1 = np.arccos(X_set['P_s'][1]) # ALPHA
+    H_n = Get_beta_j_cofactors(anti_commuting_set)
+
+    phi_n_1 = np.arccos(H_n['PauliWords'][S_index][1]) # ALPHA
 
     const = np.sin(phi_n_1 / 2) * -1j
 
@@ -428,8 +430,6 @@ def Get_R_linear_combination(anti_commuting_set, S_index, no_qubits):
     # LCU_dict['gamma_l'] = X_set['gamma_l']
 
     # THIS USES gamma_l of H_n (whereas previously was using gamma_l of H_{n-1}
-    H_n = Get_beta_j_cofactors(anti_commuting_set)
-    # LCU_dict['gamma_l'] = H_n['gamma_l']
     if X_set['P_s'][1]<0:
         LCU_dict['gamma_l'] = (H_n['gamma_l'] * -1)
     else:
@@ -1269,7 +1269,7 @@ class ALCU_Simulation_Quantum_Circuit_DictRAW():
 
         binary_results_dict = {}
         for key in self.circuits_factor_PauliWord_dict:
-
+            print(key)
             if self.hist_key_dict[key] != '':
                 #checks for non identity ciruict
                 if self.circuits_factor_PauliWord_dict[key]['LCU'] is False: