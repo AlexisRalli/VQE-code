891fc3af98d82361779b1d18308e0af727e0c434
AlexisRalli <rallilex@hotmail.co.uk>
2020-07-24 11:10:24 +0100

    started looking at Grey code implementation of LCU method... lots of Tofolli gates will cancel! For overleaf paper am also simulating Q circuits and finding total no. of measurments!

--- quchem/Scipy_Optimizer.py
+++ quchem/Scipy_Optimizer.py
@@ -430,8 +430,6 @@ if __name__ == '__main__':
 
     print(x.optimized_result)
 
-
-
 # def Adam_Opt(X_0, function, gradient_function, learning_rate=0.001, beta_1=0.9, beta_2=0.999, epsilon=1e-8, max_iter=500,
 #              disp=False, tolerance=1e-5, store_steps=False):
 #     """
@@ -536,3 +534,140 @@ if __name__ == '__main__':
 #     ax.plot_surface(x, y, z, rstride=1, cstride=1, cmap=cm.viridis)
 #     plt.show()
 #     print('Minimum should be:', 2.0)
+
+
+class Optimizer_save_no_measurements:
+    """
+
+    Base class for optimizers. To specify a new optimization technique simply define a new objective function
+
+    Args:
+        func (callable): The objective function to be minimized. funt(x, *args) -> float
+        X0 (numpy.ndarray): initial guess
+        args (tuple, optional): optional input arguements to func (NOTE these will not be optimized!) and jac, hess etc
+        method (str): Type of optimizer... custom method can be defined too
+        jac (callable): Calculates first derivative of objective function to be minimized. jac(x, *args) -> N array
+                        note keywords '2-point', '3-point', 'cs' can be used for FINITE DIFFERENCE
+
+        hess (callable, optional): Calculates Hessian matrix of objective function to be minimized. hess(x, *args) -> N x N array
+                        note keywords '2-point', '3-point', 'cs' can be used for FINITE DIFFERENCE
+
+        hessp (callable, optional): Hessian of function TIMES vector p. Returns Hess(x,p, *args) -> N array
+                                    where p is of dimention N.
+
+        bounds (sequence, optional): sequence of tuples [(lower, upper), (lower, upper) ... ] for input varibles
+
+        constraints(dict, optional): either stand along dict or list of dictionaries (for multiple constraints)
+                                     See online for further info
+
+        tol (float, optional): Tolerance for termination.
+
+        display_iter_steps (bool, optional): Whether to print each optimization step
+                                             NOTE THIS RUNS objective function... therefore can have a cost!
+
+        display_convergence_message (bool, optional): Set to True to print convergence messages.
+
+
+    Attributes:
+        iter (int): Number of iterations optimizer performed
+        obj_fun_output_values (list): list of function outputs at each optimization step.
+        obj_fun_input_vals (list): list of function inputs at each optimization step.
+
+    """
+
+    def __init__(self, func, X0, args=(), method=None, jac=None, hess=None, hessp=None, bounds=None, constraints=None,
+                 tol=None, display_convergence_message=False, display_steps=False, custom_optimizer_DICT=None):
+
+        self.func = func
+        self.X0 = X0
+        self.args = args
+        self.method = method
+        self.jac = jac
+        self.hess= hess
+        self.hessp = hessp
+        self.bounds=bounds
+        self.constraints = constraints
+        self.tol = tol
+        self.custom_optimizer_DICT = custom_optimizer_DICT
+        self.total_measurements = []
+
+        self.display_convergence_message = display_convergence_message
+        self.display_steps = display_steps
+
+        # TODO (can add hess, jac and other gradient functions to this!)
+
+    def callback_store_values(self, xk):
+        # Called after each iteration if set to store_values set too True
+
+        self.obj_fun_input_vals.append(xk)
+
+        if self.display_steps:
+            val = self.func(xk, *self.args)
+            self.obj_fun_output_values.append(val)
+
+            print(f'{self.iter}: Input_to_Funct: {xk}: Output: {val}')  # self.iters and xk
+            self.iter += 1
+
+    def get_env(self, max_iter):
+        """
+
+       Function that preforms optimization step
+
+        Args:
+            max_iter (int): Maximum number of iterations to perform. Depending on the method each iteration may
+                            use several function evaluations.
+
+        Attributes:
+            iter (int): Number of iterations optimizer performed
+            obj_fun_output_values (list): list of function outputs at each optimization step.
+            obj_fun_input_vals (list): list of function inputs at each optimization step.
+
+        Returns:
+            self.optimized_result (scipy.optimize.optimize.OptimizeResult): OptimizeResult object.
+
+        """
+
+        if self.display_steps is True:
+            self.iter=0
+            self.obj_fun_output_values = []
+
+        options = {'maxiter': max_iter,
+                   'disp': self.display_convergence_message}
+
+        kwargs = {'fun': self.func,
+                  'x0': self.X0,  # = param_obj_fun
+                  'args': self.args,
+                  'method': self.method,
+                  'jac': self.jac,
+                  'hess': self.hess,
+                  'hessp': self.hessp,
+                  'bounds': self.bounds,
+                  'constraints': self.constraints,
+                  'tol': self.tol,
+                  'options': options,
+                  'callback': self.callback_store_values }#if self.display_steps is True else None}
+
+
+        self.obj_fun_input_vals = []
+
+        if self.custom_optimizer_DICT:
+            kwargs['options'] = {**self.custom_optimizer_DICT, **options}
+
+        self.optimized_result = minimize(**kwargs)  # scipy.optimize.minimize
+
+
+        if self.display_convergence_message:
+            print(self.optimized_result.message)
+
+    def plot_convergence(self):  # , file):
+
+        if self.display_steps is True:
+            # dir_path = os.path.dirname(os.path.realpath(__file__))
+            plt.figure()
+            x = list(range(len(self.obj_fun_output_values)))
+            plt.plot(x, self.obj_fun_output_values)
+            plt.xlabel('iterations')
+            plt.ylabel('objective function value')
+            # plt.savefig(dir_path + '/' + file)
+        else:
+            raise NotImplementedError('Cannot plot convergence as values at each opt. step NOT stored')
