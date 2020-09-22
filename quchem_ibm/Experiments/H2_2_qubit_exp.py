513d144ae3f901fd3ab07465f1b794abd822ba99
AlexisRalli <rallilex@hotmail.co.uk>
2020-09-22 13:41:16 +0100

    experiments running properly... have added QASM LiH functions to make possible

--- quchem_ibm/Experiments/H2_2_qubit_exp.py
+++ quchem_ibm/Experiments/H2_2_qubit_exp.py
@@ -2,30 +2,45 @@ from quchem_ibm.IBM_experiment_functions import *
 import pickle
 import os
 import argparse
+import numpy as np
 
 
 def main(method_name):
-    molecule_name='LiH'
+    molecule_name='H2'
+
     ## Load input data
     base_dir = os.getcwd()
     data_dir = os.path.join(base_dir, 'Input_data')
-    input_file = os.path.join(data_dir, 'LiH_bravyi_kitaev_12_qubit_experiment_time=2020Sep21-132537266776.pickle')
+    input_file = os.path.join(data_dir, 'H2_bravyi_kitaev_2_qubit_experiment_time=2020Sep21-162239536536.pickle')
     with open(input_file, 'rb') as handle:
         input_data = pickle.load(handle)
 
     ## Get IBM account
     my_provider = load_IBM_provider()
     # IBM_backend = Get_IBM_backends(my_provider, show_least_busy=False)
-    IBM_backend = 'ibmq_qasm_simulator'
+    IBM_backend = 'ibmqx2'
+    # IBM_backend = None
 
-    shot_list=[8192]
 
-    run_experiment_exp_loop_QASM(molecule_name,
-                                 method_name,
+    # goes up to 4914 for standard VQE and 8190 for unitary partitioning!
+    shot_experiment_list = np.arange(1190 * 3, (8190 * 3) + 1, 2625, dtype=int)
+    if method_name == 'standard_VQE':
+        shot_list=shot_experiment_list/len(input_data['standard_VQE_circuits'])
+    else:
+        shot_list = shot_experiment_list/len(input_data['Seq_Rot_VQE_circuits'])
+
+    # shot_list=[8190 for _ in range(10)] # max for unitary part
+    # shot_list=[4914 for _ in range(10)] # max for standard vqe
+
+    n_system_qubits = input_data['n_system_qubits']
+
+    run_experiment_exp_loop(molecule_name,
+                            method_name,
                                  my_provider,
                                  IBM_backend,
                                  input_data,
                                  shot_list,
+                                 n_system_qubits,
                                  optimization_level=3)
 
 
@@ -37,3 +52,5 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     main(args.method)
+
+
