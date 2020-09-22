6588086e9b6ede328d72baadac30bbf5a5e93a18
AlexisRalli <rallilex@hotmail.co.uk>
2020-09-22 13:41:16 +0100

    experiments running properly... have added QASM LiH functions to make possible

--- quchem_ibm/IBM_experiment_functions.py
+++ quchem_ibm/IBM_experiment_functions.py
@@ -1,5 +1,5 @@
 import argparse
-from qiskit import QuantumRegister, execute, Aer
+from qiskit import QuantumRegister, execute, Aer, QuantumCircuit
 import datetime
 from qiskit.compiler import transpile, assemble
 import pickle
@@ -7,6 +7,7 @@ import os
 from qiskit.providers.ibmq import least_busy
 from qiskit import IBMQ
 from qiskit.ignis.mitigation.measurement import complete_meas_cal,CompleteMeasFitter
+from tqdm import tqdm
 
 def load_IBM_provider(IBM_key=None):
     try:
@@ -34,6 +35,14 @@ def Get_IBM_backends(IBM_provider, show_least_busy=False):
 
     return IBM_backend
 
+
+def retrieve_job(job_id):
+    provider = load_IBM_provider()
+    backend = provider.backends.ibmq_qasm_simulator
+    retrieved_job = backend.retrieve_job(job_id)
+    print(retrieved_job.status())
+    return retrieved_job
+
 def Get_Measurement_Filter(n_qubits, backend_name, n_shots, my_provider=None):
     # Error mitigation
     qr_standard = QuantumRegister(n_qubits)
@@ -52,7 +61,7 @@ def Get_Measurement_Filter(n_qubits, backend_name, n_shots, my_provider=None):
     meas_filter = meas_fitter.filter
     return meas_filter
 
-def run_VQE_unitary_part_experiments(method_name, qc_circuit_dictionary_list, my_provider, n_shots, n_system, I_term,
+def run_VQE_unitary_part_experiments(molecule_name, method_name, qc_circuit_dictionary_list, my_provider, n_shots, n_system, I_term,
                                      save=True, optimization_level=3, backend_name=None):
 
     backend_simulator = Aer.get_backend('qasm_simulator')
@@ -65,7 +74,7 @@ def run_VQE_unitary_part_experiments(method_name, qc_circuit_dictionary_list, my
     if backend_name:
         transpiled_circs = transpile(qc_list, backend=backend, optimization_level=optimization_level)
 
-
+    transpiled_circs_sim=None
     if backend_name:
         if method_name == 'LCU':
             if backend_name:
@@ -79,6 +88,8 @@ def run_VQE_unitary_part_experiments(method_name, qc_circuit_dictionary_list, my
         transpiled_circs_sim = transpile(qc_list, backend=backend_simulator, optimization_level=None)
         qobjs_sim = assemble(transpiled_circs_sim, backend=backend_simulator, shots=n_shots,
                              memory=True if method_name == 'LCU' else False)
+
+    transpiled_circs_sim = transpiled_circs if transpiled_circs_sim is None else transpiled_circs_sim
     ## error mitigation
     # https://qiskit.org/textbook/ch-quantum-hardware/measurement-error-mitigation.html
     if backend_name:
@@ -144,7 +155,7 @@ def run_VQE_unitary_part_experiments(method_name, qc_circuit_dictionary_list, my
     if save:
         print('## saving job ## \n')
         time = datetime.datetime.now().strftime('%Y%b%d-%H%M%S%f')
-        F_name = 'method={}___time={}'.format(method_name, time)
+        F_name = 'molecule={}___n_shots={}___method={}___time={}'.format(molecule_name, n_shots, method_name, time)
 
         #         base_dir = os.path.dirname(os.path.realpath(__file__))
         base_dir = os.getcwd()
@@ -165,3 +176,170 @@ def run_VQE_unitary_part_experiments(method_name, qc_circuit_dictionary_list, my
         print('experiment data saved here: {}'.format(filepath))
 
     return output
+
+def run_experiment_exp_loop(molecule_name, method_name, my_provider, backend_name, INPUT_dict, shot_list, n_system,
+                            optimization_level=3):
+
+    if method_name == 'standard_VQE':
+        qc_circuit_dictionary_list = INPUT_dict['standard_VQE_circuits']
+        I_term = INPUT_dict['standard_I_term']
+
+    elif method_name== 'seq_rot_VQE':
+        qc_circuit_dictionary_list = INPUT_dict['Seq_Rot_VQE_circuits']
+        I_term = INPUT_dict['Seq_Rot_I_term']
+
+    elif method_name== 'LCU':
+        qc_circuit_dictionary_list = INPUT_dict['LCU_VQE_circuits']
+        I_term = INPUT_dict['LCU_I_term']
+    else:
+        raise ValueError('unknown method {}'.format(method_name))
+
+    for n_shots in tqdm(shot_list, ascii=True, desc='Running experiments'):
+    # for n_shots in shot_list:
+        run_VQE_unitary_part_experiments(molecule_name,
+                                        method_name,
+                                        qc_circuit_dictionary_list,
+                                        my_provider,
+                                        n_shots,
+                                        n_system,
+                                        I_term,
+                                        save = True,
+                                        optimization_level = optimization_level,
+                                        backend_name = backend_name)
+
+
+
+def run_VQE_unitary_part_experiments_QASM(molecule_name, method_name, qc_circuit_dictionary_list, my_provider, n_shots, I_term, backend_name,
+                                     save=True, optimization_level=3):
+
+    backend = my_provider.get_backend(backend_name)
+
+    output = {}
+
+    # QASM PART!
+    qc_list = [QuantumCircuit.from_qasm_str(exp_dict['circuit']) for exp_dict in tqdm(qc_circuit_dictionary_list, ascii=True, desc='Getting Q Circuits') if exp_dict['circuit']]
+    # qc_list = [QuantumCircuit.from_qasm_str(exp_dict['circuit']) for exp_dict in qc_circuit_dictionary_list if exp_dict['circuit']]
+    transpiled_circs = transpile(qc_list, backend=backend, optimization_level=optimization_level)
+    del qc_list
+
+    if method_name == 'LCU':
+        qobjs = assemble(transpiled_circs, backend=backend, shots=n_shots, memory=True)
+    else:
+        qobjs = assemble(transpiled_circs, backend=backend, shots=n_shots)
+
+    transpiled_circs_length= len(transpiled_circs)
+    del transpiled_circs
+
+    # ## error mitigation
+    # # https://qiskit.org/textbook/ch-quantum-hardware/measurement-error-mitigation.html
+    # if backend_name:
+    #     max_shots = 8192
+    #     if method_name == 'LCU':
+    #         LCU_meas_filters=[]
+    #         N_ancilla_per_exp = set([exp_dict['N_ancilla'] for exp_dict in qc_circuit_dictionary_list if 'N_ancilla' in exp_dict.keys()])
+    #         for n_ancilla in N_ancilla_per_exp:
+    #             #fitter is n_ancilla + n_system!
+    #             LCU_meas_filters.append({'N_ancilla': n_ancilla, 'filter': Get_Measurement_Filter(n_ancilla+n_system, backend_name, max_shots, my_provider=my_provider)})
+    #
+    #     stnd_meas_filter = Get_Measurement_Filter(n_system, backend_name, max_shots, my_provider=my_provider)
+    # #### end error mitigation
+
+    output['start_time'] = datetime.datetime.now()
+    print('## running job ## \n')
+    job = backend.run(qobjs)
+
+    # if (job.status().name != 'DONE') and (backend.configuration().simulator is not True):
+    #     try:
+    #         print('queue position = ', job.queue_position())
+    #     except:
+    #         print('could not find queue position')
+    ###
+    # while job.status().name != 'DONE':
+    #     try:
+    #         print('queue position = ', job.queue_position())
+    #         time.sleep(60*5)
+    #     except:
+    #         break
+
+    print('## job finished ## \n')
+
+    output['end_time'] = datetime.datetime.now()
+    output['method'] = method_name
+    output['n_shots'] = n_shots
+    output['I_term'] = I_term
+    # output['q_circuit_list'] = qc_list # from theory
+    # output['SIM_transpiled_q_circuit_list'] = transpiled_circs_sim #transpiled_circs_sim
+    output['experiment_dict'] = qc_circuit_dictionary_list
+    # output['SIM_count_list_raw'] = [sim_job.result().get_counts(i) for i in range(len(transpiled_circs_sim))]
+    if backend_name:
+        output['jobID'] = job.job_id()
+        # output['EXP_transpiled_q_circuit_list'] = transpiled_circs  # for chip
+        output['EXP_backend'] = backend.name()
+        # output['EXP_count_list_raw'] = [job.result().get_counts(transpiled_circ) for transpiled_circ in
+        #                            transpiled_circs]
+        output['EXP_count_list_raw'] = [job.result().get_counts(i) for i in range(transpiled_circs_length)]
+        # output['standard_measurement_filter'] = stnd_meas_filter
+
+    if method_name == 'LCU':
+        # output['SIM_memory_raw'] = [sim_job.result().get_memory(transpiled_circ) for transpiled_circ in
+        #                             transpiled_circs_sim]
+        # output['SIM_memory_raw'] = [sim_job.result().get_memory(i) for i in range(len(transpiled_circs_sim))]
+        if backend_name:
+            # output['EXP_memory_raw'] = [job.result().get_memory(transpiled_circs[transpiled_circ]) for transpiled_circ in
+            #                         transpiled_circs]
+            output['EXP_memory_raw'] = [job.result().get_memory(i) for i in range(transpiled_circs_length)]
+            # output['LCU_measurement_filters'] = LCU_meas_filters
+
+    if save:
+        print('## saving job ## \n')
+        time = datetime.datetime.now().strftime('%Y%b%d-%H%M%S%f')
+        F_name = 'molecule={}___n_shots={}___method={}___time={}'.format(molecule_name, n_shots, method_name, time)
+
+        #         base_dir = os.path.dirname(os.path.realpath(__file__))
+        base_dir = os.getcwd()
+        filepath = os.path.join(base_dir, 'IBM_qasm_simulator')
+
+        if not os.path.exists(filepath):
+            os.makedirs(filepath)
+
+        filepath = os.path.join(filepath, F_name)
+        with open(filepath + '.pickle', 'wb') as fhandle:
+            pickle.dump(output, fhandle, protocol=pickle.HIGHEST_PROTOCOL)
+
+        print('experiment data saved here: {}'.format(filepath))
+
+    return output
+
+
+
+
+def run_experiment_exp_loop_QASM(molecule_name, method_name, my_provider, backend_name, INPUT_dict, shot_list,
+                            optimization_level=3):
+
+    if method_name== 'standard_VQE':
+        qc_circuit_dictionary_list = INPUT_dict['standard_VQE_circuits']
+        I_term = INPUT_dict['standard_I_term']
+
+    elif method_name== 'seq_rot_VQE':
+        qc_circuit_dictionary_list = INPUT_dict['Seq_Rot_VQE_circuits']
+        I_term = INPUT_dict['Seq_Rot_I_term']
+
+    elif method_name== 'LCU':
+        qc_circuit_dictionary_list = INPUT_dict['LCU_VQE_circuits']
+        I_term = INPUT_dict['LCU_I_term']
+    else:
+        raise ValueError('unknown method {}'.format(method_name))
+
+
+    for n_shots in tqdm(shot_list, ascii=True, desc='Running experiments'):
+    # for n_shots in shot_list:
+        run_VQE_unitary_part_experiments_QASM(molecule_name,
+                                              method_name,
+                                              qc_circuit_dictionary_list,
+                                              my_provider,
+                                              n_shots,
+                                              I_term,
+                                              backend_name,
+                                              save=True,
+                                              optimization_level=optimization_level)
+
--- quchem_ibm/Qiskit_Chemistry.py
+++ quchem_ibm/Qiskit_Chemistry.py
@@ -11,6 +11,14 @@ import pickle
 import os
 import datetime
 
+def Total_decompose_qiskit(qc):
+    while True:
+        qc_d = qc.decompose()
+        if qc_d == qc:
+            return qc_d
+        else:
+            qc = qc_d
+
 def HF_state_IBM_circuit(HF_state, q_register, q_circuit):
     ## re-order IN REVERSE!!!!!!!!!!!!!! IMPORTANT!!!!!
 
@@ -188,7 +196,7 @@ def IBM_PauliWord(PauliOp, N_qubits, draw=False, reverse=False):
         return Operator(q_circuit).data
 
 
-def Vector_defined_Ansatz(n_qubits, ground_state_vector, check_ansatz_state=False):
+def Vector_defined_Ansatz(n_qubits, ground_state_vector, check_ansatz_state=False, decompose_fully=False):
     q_reg = QuantumRegister(n_qubits)
     qcirc = QuantumCircuit(q_reg)
 
@@ -198,6 +206,9 @@ def Vector_defined_Ansatz(n_qubits, ground_state_vector, check_ansatz_state=Fals
                                                     check_state=check_ansatz_state)
 
     ansatz_circ = ansatz_circ.reverse_bits()
+
+    if decompose_fully:
+        ansatz_circ = Total_decompose_qiskit(ansatz_circ)
 
     return ansatz_circ, q_reg
 
@@ -220,35 +231,24 @@ def Build_Standard_VQE_circuits(QubitHamiltonian, Ansatz_circuit, q_reg):
                 # circuit_list.append({'circuit': None, 'coeff': const})
     return circuit_list, I_term
 
-# def Build_Standard_VQE_circuits_MEMORY_EFF(QubitHamiltonian, Ansatz_circuit, q_reg, filename):
-#
-#     time = datetime.datetime.now().strftime('%Y%b%d-%H%M%S%f')
-#     F_name = '{}_time={}'.format(filename, time)
-#     base_dir = os.getcwd()
-#     input_dir = os.path.join(base_dir, 'saved_circuits')
-#     if not os.path.exists(input_dir):
-#         os.makedirs(input_dir)
-#
-#     # filepath = os.path.join(input_dir, F_name)
-#     #
-#     # with open(filepath + '.pickle', 'wb') as fhandle:
-#     #     pickle.dump(output, fhandle, protocol=pickle.HIGHEST_PROTOCOL)
-#
-#
-#     circuit_list=[]
-#     for qubitOp in tqdm(QubitHamiltonian, ascii=True, desc='Getting_standard_VQE_circuits'):
-#         for PauliWord, const in qubitOp.terms.items():
-#             if PauliWord:
-#                 full_circuit = change_basis_for_Z_measure(qubitOp,
-#                                                           q_reg,
-#                                                           Ansatz_circuit,
-#                                                           )
-#                 full_circuit.measure_all()
-#                 circuit_list.append({'circuit': full_circuit, 'coeff': const, 'qubitOp': qubitOp})
-#             else:
-#                 I_term = const
-#                 # circuit_list.append({'circuit': None, 'coeff': const})
-#     return circuit_list, I_term
+def Build_Standard_VQE_circuits_MEMORY_EFF(QubitHamiltonian, Ansatz_circuit, q_reg):
+
+    circuit_list=[]
+    for qubitOp in tqdm(QubitHamiltonian, ascii=True, desc='Getting_standard_VQE_circuits'):
+        for PauliWord, const in qubitOp.terms.items():
+            if PauliWord:
+                full_circuit = change_basis_for_Z_measure(qubitOp,
+                                                          q_reg,
+                                                          Ansatz_circuit,
+                                                          )
+                full_circuit.measure_all()
+                qasm_circuit = full_circuit.qasm()
+                del full_circuit
+                circuit_list.append({'circuit': qasm_circuit, 'coeff': const, 'qubitOp': qubitOp})
+            else:
+                I_term = const
+                # circuit_list.append({'circuit': None, 'coeff': const})
+    return circuit_list, I_term
 
 def standard_VQE_lin_alg(QubitHamiltonian, ground_state_vector, n_qubits, check_ansatz_state=False):
 
@@ -356,6 +356,59 @@ def Get_Seq_Rot_Unitary_Part_circuits(anti_commuting_sets, Ansatz_circuit, q_reg
                                                               Ansatz_circuit)
                     full_circuit.measure_all()
                     circuit_list.append({'circuit': full_circuit, 'coeff': const, 'qubitOp': qubitOp})
+                else:
+                    I_term = const
+                    # circuit_list.append({'circuit': None, 'coeff': const})
+    return circuit_list, I_term
+
+def Get_Seq_Rot_Unitary_Part_circuits_MEMORY_EFF(anti_commuting_sets, Ansatz_circuit, q_reg, n_qubits, S_index_dict=None,
+                                      rotation_reduction_check=False):
+
+
+    if S_index_dict is None:
+        S_index_dict = {key: 0 for key in anti_commuting_sets}
+
+
+    circuit_list = []
+    for set_key in tqdm(list(anti_commuting_sets.keys()), ascii=True, desc='Getting seq_rot VQE circuits'):
+        anti_set_list = anti_commuting_sets[set_key]
+        if len(anti_set_list) > 1:
+            R_sl_circuit, Ps, gamma_l = Build_reduction_circuit_seq_rot_IBM(
+                                                                            anti_set_list,
+                                                                            S_index_dict[set_key],
+                                                                            q_reg,
+                                                                            n_qubits,
+                                                                            check_reduction=rotation_reduction_check)
+
+            GATES = ['u1', 'u2', 'u3', 'cx']
+            R_sl_circuit = transpile(R_sl_circuit,
+                                     backend=None,
+                                     basis_gates=GATES)
+
+            combined_circuits = Ansatz_circuit.combine(R_sl_circuit)
+
+            full_circuit = change_basis_for_Z_measure(Ps,
+                                                      q_reg,
+                                                      combined_circuits)
+            full_circuit.measure_all()
+
+            qasm_circuit = full_circuit.qasm()
+            del full_circuit
+
+            circuit_list.append({'circuit': qasm_circuit, 'gamma_l': gamma_l, 'Ps': Ps})
+        else:
+            qubitOp = anti_set_list[0]
+            for PauliWord, const in qubitOp.terms.items():
+                if PauliWord:
+                    full_circuit = change_basis_for_Z_measure(qubitOp,
+                                                              q_reg,
+                                                              Ansatz_circuit)
+                    full_circuit.measure_all()
+
+                    qasm_circuit = full_circuit.qasm()
+                    del full_circuit
+
+                    circuit_list.append({'circuit': qasm_circuit, 'coeff': const, 'qubitOp': qubitOp})
                 else:
                     I_term = const
                     # circuit_list.append({'circuit': None, 'coeff': const})
@@ -489,6 +542,23 @@ def Get_post_selection_counts_LCU(list_of_measurements, N_ancilla):
 
     return new_counts
 
+def Get_post_selection_counts_DICT_LCU(count_dict, N_ancilla):
+    # checks all zero state on ancilla line
+    # ancilla must be first part of measurment string!
+    # requires circuit simulation memory=True
+
+    new_counts = {}
+    for binary_result_str in count_dict:
+        ancilla_state = int(binary_result_str[:N_ancilla], 2)
+        if ancilla_state == 0:
+            post_select_m_binary = binary_result_str[N_ancilla:]
+            new_counts[post_select_m_binary] = count_dict[binary_result_str]
+        else:
+            continue
+
+    return new_counts
+
+
 def Get_LCU_Unitary_Part_circuits(anti_commuting_sets, Ansatz_circuit, q_reg, n_qubits,
                                   N_index_dict=None):
 
@@ -557,6 +627,91 @@ def Get_LCU_Unitary_Part_circuits(anti_commuting_sets, Ansatz_circuit, q_reg, n_
                     # circuit_list.append({'circuit': None, 'coeff': const})
     return circuit_list, I_term
 
+
+from qiskit.compiler import transpile
+def Get_LCU_Unitary_Part_circuits_MEMORY_EFF(anti_commuting_sets, Ansatz_circuit, q_reg, n_qubits,
+                                             N_index_dict=None):
+    if N_index_dict is None:
+        N_index_dict = {key: 0 for key in anti_commuting_sets}
+
+    circuit_list = []
+    for set_key in tqdm(list(anti_commuting_sets.keys()), ascii=True, desc='Getting LCU VQE circuits'):
+        anti_set_list = anti_commuting_sets[set_key]
+        if len(anti_set_list) > 1:
+            R_uncorrected, Pn, gamma_l = Get_R_op_list(anti_set_list, N_index_dict[set_key])
+            R_corrected_Op_list, R_corr_list, ancilla_amplitudes, l1 = absorb_complex_phases(R_uncorrected)
+
+            N_ancilla = int(np.ceil(np.log2(len(ancilla_amplitudes))))
+            if len(ancilla_amplitudes) != 2 ** N_ancilla:
+                n_missing = int(2 ** N_ancilla - len(ancilla_amplitudes))
+                missing_terms = [0 for _ in range(n_missing)]
+                ancilla_amplitudes = [*ancilla_amplitudes, *missing_terms]
+
+            q_reg_ancilla = QuantumRegister(N_ancilla)
+            q_circ_ancilla = QuantumCircuit(q_reg_ancilla)
+
+            G_circuit = Get_Q_circ_to_build_state(ancilla_amplitudes, q_reg_ancilla, q_circ_ancilla)
+            G_circuit = Total_decompose_qiskit(G_circuit)
+
+            G_inverse = G_circuit.inverse()
+
+            # combine ancilla and system
+            combined_circuits = Ansatz_circuit.combine(G_circuit)
+
+            # find qubits that are measured!
+            Pn_qubitNos, _ = zip(*list(*Pn.terms.keys()))
+
+            for control_index, op in enumerate(R_corrected_Op_list):
+                phase_corr = R_corr_list[control_index]
+                for PauliW, Const in op.terms.items():
+                    if PauliW:
+                        combined_circuits = control_P_IBM(op,
+                                                          phase_corr,
+                                                          control_index,
+                                                          combined_circuits,
+                                                          n_qubits,
+                                                          N_ancilla,
+                                                          list_measured_qubits=Pn_qubitNos)
+                    else:
+                        continue
+
+            # G dag
+            combined_circuits = combined_circuits.combine(G_inverse)
+
+            # decomposed
+            combined_circuits = Total_decompose_qiskit(combined_circuits)
+
+            full_circuit = change_basis_for_Z_measure(Pn,
+                                                      q_reg,
+                                                      combined_circuits)
+            full_circuit.measure_all()
+
+            GATES = ['u1', 'u2', 'u3', 'cx']
+            full_circuit = transpile(full_circuit,
+                                     backend=None,
+                                     basis_gates=GATES)
+
+            qasm_circuit = full_circuit.qasm()
+            del full_circuit
+
+            circuit_list.append({'circuit': qasm_circuit, 'gamma_l': gamma_l, 'Pn': Pn, 'N_ancilla': N_ancilla})
+        else:
+            qubitOp = anti_set_list[0]
+            for PauliWord, const in qubitOp.terms.items():
+                if PauliWord:
+                    full_circuit = change_basis_for_Z_measure(qubitOp,
+                                                              q_reg,
+                                                              Ansatz_circuit)
+                    full_circuit.measure_all()
+
+                    qasm_circuit = full_circuit.qasm()
+                    del full_circuit
+
+                    circuit_list.append({'circuit': qasm_circuit, 'coeff': const, 'qubitOp': qubitOp})
+                else:
+                    I_term = const
+                    # circuit_list.append({'circuit': None, 'coeff': const})
+    return circuit_list, I_term
 
 def POVM_LCU(n_system_q, n_ancilla_q, system_ancilla_output_ket):
     # state_vector_simulator the state is given as (ancilla X_kron system)
